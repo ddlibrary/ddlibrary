6ee908034aa579a911cba0231c745e456d7d55e9
Jamshid Hashimi <jamshidhashimi@gmail.com>
2018-04-29 15:47:37 -0700

    auth uses database

--- resources/views/admin/layouts/navigation.blade.php
+++ resources/views/admin/layouts/navigation.blade.php
@@ -1,6 +1,6 @@
 <!-- Navigation-->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
-    <a class="navbar-brand" href="index.html">DDL Admin Panel</a>
+<a class="navbar-brand" href="{{ URL::to('/admin') }}">DDL Admin Panel</a>
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
