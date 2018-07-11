38bb33e9380a3d6a280a5f39f8b4032baea1275a
Jamshid Hashimi <jamshidhashimi@gmail.com>
2018-07-10 18:06:12 -0700

    fixed: Clicking Dashboard in the navbar should return to Dashboard but does nothing

--- resources/views/admin/resources/resources.blade.php
+++ resources/views/admin/resources/resources.blade.php
@@ -5,7 +5,7 @@
     <!-- Breadcrumbs-->
     <ol class="breadcrumb">
       <li class="breadcrumb-item">
-        <a href="#">Dashboard</a>
+        <a href="{{ URL::to('admin') }}">Dashboard</a>
       </li>
       <li class="breadcrumb-item active">Resources</li>
     </ol>
