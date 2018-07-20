ad13f3bd765c77066bd6c74a9a3528b9ebc9bb4a
Jamshid Hashimi <jamshidhashimi@gmail.com>
2018-07-20 05:52:08 -0700

    Safari search box bug resolved

--- resources/views/layouts/search.blade.php
+++ resources/views/layouts/search.blade.php
@@ -4,7 +4,7 @@
     </header>
     <form method="POST" action="{{ route('resourceList') }}">
         @csrf
-        <input type="search" name="search" class="form-search" value="{{ session('search') }}" placeholder="Search our library">
+        <input type="search" name="search" class="form-search form-control" value="{{ session('search') }}" placeholder="Search our library" autofocus>
         <input type="submit" class="search-button" value="Go">
     </form>
 </section>
