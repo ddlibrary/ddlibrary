93535b4aa2e89083140b60b5ba6f8d027c66206a
Jamshid Hashimi <jamshidhashimi@gmail.com>
2018-08-03 16:29:21 -0700

    css fix

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
     <link rel="stylesheet" href="{{ asset('css/common.css') }}">
     <link rel="stylesheet" href="{{ asset('css/ddl.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/fontawesome-all.min.css') }}">
     @stack('styles')
 
     @if(env('APP_ENV') === 'production')
