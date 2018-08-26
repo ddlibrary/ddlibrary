1ec4972cd0353ac5f12e56224ef80b6aa25ea356
Jamshid Hashimi <jamshidhashimi@gmail.com>
2018-08-25 19:25:56 -0700

    menu edit

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -26,4 +26,38 @@ class MenuController extends Controller
         $menuRecords = Menu::paginate(10);
         return view('admin.menu.menu_list', compact('menuRecords'));
     }
+
+    function edit(Menu $menu, $menuId)
+    {
+        $details = $menu->find($menuId);
+        $locations = $details->distinct()->pluck('location');
+        $parents = $details->distinct()->pluck('title','id');
+        return view('admin.menu.menu_edit', compact('details','locations','parents'));
+    }
+
+    public function update(Request $request, $menuId)
+    {
+        $this->validate($request, [
+            'title'      => 'required',
+            'location'   => 'required',
+            'path'    => 'required',
+            'parent'       => 'nullable',
+            'language'  => 'required',
+            'weight'  => 'required'
+        ]);
+
+        $menu = Menu::find($menuId);
+        $menu->title = $request->input('title');
+        $menu->location = $request->input('location');
+        $menu->path = $request->input('path');
+        if($request->filled('parent')){
+            $menu->parent = $request->input('parent');
+        }
+        $menu->language = $request->input('language');
+        $menu->weight = $request->input('weight');
+        //inserting
+        $menu->save();
+
+        return redirect('admin/menu/edit/'.$menuId)->with('success', 'Item successfully updated!');
+    }
 }
