5f3dd32fc645257772f8e5bc0b48fca4a10f6c53
Jamshid Hashimi <jamshidhashimi@gmail.com>
2018-10-13 12:48:05 +0430

    removed the test api part

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Http\Request;
-use App\User;
-use App\Resource;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,26 +11,3 @@ use App\Resource;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-if (env('APP_ENV') !== 'production') {
-	Route::get('usersss', function() {
-	    // If the Content-Type and Accept headers are set to 'application/json', 
-	    // this will return a JSON structure. This will be cleaned up later.
-	    $users = User::all();
-
-	    return response()->json($users);
-	});
-
-	Route::get('resources', function() {
-	    // If the Content-Type and Accept headers are set to 'application/json', 
-	    // this will return a JSON structure. This will be cleaned up later.
-	    $resources = Resource::where('language','fa')->where('status', 1)->orderBy('id','desc')->get();
-
-	    return response()->json($resources);
-	});
-
-	Route::get('userrr/{id}', function($id) {
-	    $user = User::find($id);
-
-	    return response()->json($user);
-	});
-}
