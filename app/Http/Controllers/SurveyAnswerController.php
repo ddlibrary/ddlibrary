20375575bf6e069eda9ec7d68169180aac01306e
shakour-ghafarzoy <ghafarzoy.shakour@gmail.com>
2018-12-01 23:53:24 +0430

    Survey interface and functionalities has been changed.

--- app/Http/Controllers/SurveyAnswerController.php
+++ app/Http/Controllers/SurveyAnswerController.php
@@ -5,11 +5,4 @@ use App\SurveyAnswer;
 use Illuminate\Http\Request;
 
 class SurveyAnswerController extends Controller
-{
-    public function index()
-    {
-        $this->middleware('admin');
-        $survey_answers = SurveyAnswer::all();
-        return view('admin.surveys.survey_answers', compact('survey_answers'));        
-    }
-}
+{}
--- app/Http/Controllers/SurveyQuestionController.php
+++ app/Http/Controllers/SurveyQuestionController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\SurveyAnswer;
+use App\SurveyQuestion;
+use App\SurveyQuestionOption;
 use Illuminate\Http\Request;
 
 class SurveyQuestionController extends Controller
 {
-    //
+
+	public function index()
+    {
+        $this->middleware('admin');
+        $survey_questions = SurveyQuestion::all();
+        return view('admin.surveys.survey_questions', compact('survey_questions'));        
+    }
+
+    public function viewAnswers($id)
+    {
+    	$this->middleware('admin');
+    	$question = SurveyQuestion::find($id);
+    	$survey_question_options = SurveyQuestionOption::where('question_id', $id)->get();
+    	return view('admin.surveys.question_answers', compact('question','survey_question_options')); 
+    }
+
 }
