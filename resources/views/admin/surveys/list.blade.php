8db50472e7ce75acea19d76423812aedf696b67e
shakour-ghafarzoy <ghafarzoy.shakour@gmail.com>
2018-12-17 00:06:05 +0430

    Modified survey setting view, removed useless migration and a confirmation will be need while deleting a survey.

--- resources/views/admin/surveys/list.blade.php
+++ resources/views/admin/surveys/list.blade.php
@@ -19,6 +19,11 @@
         </div>
 
         <div class="card-body">
+            @if (session('status'))
+              <div class="alert alert-success">
+                  {{ session('status') }}
+              </div>
+            @endif
             <a href="{{ URL::to('admin/survey/create') }}" class="btn btn-success pull-right" style="margin-bottom: 10px">
               <span class="fa fa-plus"></span> Add New
             </a>
@@ -40,7 +45,7 @@
                     <td style="display: flex;">
                       <a href="survey/questions/{{$survey->id}}" class="badge badge-primary" style="margin-right: 5px;">Questions</a>
                       <a href="survey/edit/{{$survey->id}}" class="badge badge-primary" style="margin-right: 5px;">Edit</a>
-                      <a href="survey/delete/{{$survey->id}}" class="badge badge-danger">Delete</a>
+                      <a href="javascript:void(0)" id="{{$survey->id}}" onclick="confirm(this.id);" class="badge badge-danger">Delete</a>
                     </td>
                   </tr>
                 @endforeach
@@ -48,9 +53,41 @@
             </table>
           </div>
         </div>
-
       </div>
     </div>
     <!-- /.container-fluid-->
     <!-- /.content-wrapper-->
-  @endsection
+
+    <!-- Modal for confirmation -->
+    <div class="modal fade" id="confirmModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Confirmation</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <p>Are you sure want to delete this survey?</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-success btn-sm" data-dismiss="modal">No</button>
+            <a class="delete"><button type="button" class="btn btn-danger btn-sm confirm">Yes</button></a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!--javascript function for deleting a survey/starts-->
+    <script>
+        function confirm(id){
+            $("#confirmModal").modal('show');
+            $('.confirm').click(function(){
+                var url='/admin/survey/delete/'+id;
+                $('a.delete').attr('href',url);
+            });
+        }
+    </script>
+    <!--end-->
+@endsection
