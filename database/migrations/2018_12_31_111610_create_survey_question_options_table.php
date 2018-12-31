c7dabe7029264588b50c8ece2f40360990e6e52a
shakour-netlinks <shakour@netlinks.ws>
2018-12-31 16:14:44 +0430

    Added migrations for surveys table.

---
+++ database/migrations/2018_12_31_111610_create_survey_question_options_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSurveyQuestionOptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('survey_question_options', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('text');
+            $table->foreign('question_id')->references('id')->on('survey_questions')->onDelete('`cascade`');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('survey_question_options');
+    }
+}
---
+++ database/migrations/2018_12_31_111707_create_survey_answers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSurveyAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('survey_answers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreign('question_id')->references('id')->on('survey_questions')->onDelete('cascade');
+            $table->foreign('answer_id')->references('id')->on('survey_question_options')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('survey_answers');
+    }
+}
