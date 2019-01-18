3ff3dca17c770f83dbe3d846ce428954aed5f113
Jamshid Hashimi <jamshidhashimi@gmail.com>
2019-01-18 15:27:49 -0800

    laravel mix setup

--- resources/views/resources/resources_add_step2.blade.php
+++ resources/views/resources/resources_add_step2.blade.php
@@ -154,7 +154,6 @@
     </div>
 </section>
 @push('scripts')
-<script src="{{ asset('js/ddl.js') }}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
 
 <script>
