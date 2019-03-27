050a74105f4b5a21ce632e618ddf602c3dc27401
Jamshid Hashimi <jamshidhashimi@gmail.com>
2019-03-26 19:13:43 -0700

    Updated placeholder for search

--- resources/views/layouts/search.blade.php
+++ resources/views/layouts/search.blade.php
@@ -5,7 +5,7 @@
     <form method="GET" action="{{ route('resourceList') }}" id="search-form">
         @csrf
         <label for="search">
-            <input type="search" name="search" class="form-search form-control" value="{{ session('search') }}" placeholder="@lang('Search our library')" autofocus>
+            <input type="search" name="search" class="form-search form-control" value="{{ session('search') }}" placeholder="@lang('SEARCH OUR GROWING LIBRARY!')" autofocus>
         </label>
         <input type="submit" class="search-button" value="@lang('Go')">
     </form>
