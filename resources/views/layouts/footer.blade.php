83cec3b4a38477c434b4299c639da229e9d0a50e
Jamshid Hashimi <jamshidhashimi@gmail.com>
2019-04-22 09:57:14 -0700

    Updated the Facebook URL

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -7,7 +7,7 @@
     </nav>
     <div>
         <i class="fab fa-twitter fa-2x" title="DDL Twitter Account" onclick="window.location.href='https://twitter.com/AfghanOERs'"></i>
-        <i class="fab fa-facebook fa-2x" title="DDL Facebook Account" onclick="window.location.href='https://www.facebook.com/darakhtdanesh'"></i>
+        <i class="fab fa-facebook fa-2x" title="DDL Facebook Account" onclick="window.location.href='https://www.facebook.com/AfghanOERs/'"></i>
         <i class="fab fa-youtube fa-2x" title="DDL Youtube Account" onclick="window.location.href='https://www.youtube.com/channel/UCVmc4QsedamLXMeXbutW-iw/videos'"></i>
     </div>
 </footer>
