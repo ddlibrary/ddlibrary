bc83d1e61dfb5a7e91f611e258a511300a818567
Jamshid Hashimi <jamshidhashimi@gmail.com>
2019-04-23 09:59:21 -0700

    Removed Drift and add Facebook Messenger Support

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -23,35 +23,6 @@
     <link rel="stylesheet" href="{{ asset('css/all.css') }}">
 
     @stack('styles')
-
-    <!-- Start of Async Drift Code -->
-    <script>
-        "use strict";
-
-        !function() {
-        var t = window.driftt = window.drift = window.driftt || [];
-        if (!t.init) {
-            if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
-            t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ], 
-            t.factory = function(e) {
-            return function() {
-                var n = Array.prototype.slice.call(arguments);
-                return n.unshift(e), t.push(n), t;
-            };
-            }, t.methods.forEach(function(e) {
-            t[e] = t.factory(e);
-            }), t.load = function(t) {
-            var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
-            o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
-            var i = document.getElementsByTagName("script")[0];
-            i.parentNode.insertBefore(o, i);
-            };
-        }
-        }();
-        drift.SNIPPET_VERSION = '0.3.1';
-        drift.load('r5r6mvw2c92i');
-    </script>
-    <!-- End of Async Drift Code -->
     @if(App::environment('production'))
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-6207513-43"></script>
@@ -75,6 +46,36 @@
     </script>
 </head>
 <body>
+
+    <!-- Facebook Chat Integration - Start -->
+    <!-- Load Facebook SDK for JavaScript -->
+    <div id="fb-root"></div>
+    <script>
+        window.fbAsyncInit = function() {
+            FB.init({
+            xfbml            : true,
+            version          : 'v3.2'
+            });
+        };
+
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
+
+    <!-- Your customer chat code -->
+    <div class="fb-customerchat"
+        attribution=setup_tool
+        page_id="1540661852875335"
+        theme_color="#ffa800"
+        logged_in_greeting="به کتاب‌خانه درخت دانش خوش آمدید. چگونه می‌توانیم به شما کمک کنیم؟"
+        logged_out_greeting="به کتاب‌خانه درخت دانش خوش آمدید. چگونه می‌توانیم به شما کمک کنیم؟">
+    </div>
+    <!-- Facebook Chat Integration - End -->
     <div class="se-pre-con"></div>
     @include('layouts.banner')
     @yield('search')
