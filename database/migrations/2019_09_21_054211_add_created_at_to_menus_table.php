528e665953b590d4009a323d36c5c198ea4be5cc
Faisal Abubaker <ahmadfaisal47@gmail.com>
2019-09-21 10:26:22 +0430

    Menus table new column migration

---
+++ database/migrations/2019_09_21_054211_add_created_at_to_menus_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCreatedAtToMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('menus', function (Blueprint $table) {
+            //
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('menus', function (Blueprint $table) {
+            //
+            $table->dropColumn('created_at');
+        });
+    }
+}
