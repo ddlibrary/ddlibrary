93244f4e2b3d7b9ea979d236521343de9c51488e
Mujib Halimi <46517293+mujibhalimi@users.noreply.github.com>
2019-11-06 02:03:21 +0430

    Add files via upload

--- routes/api.php
+++ routes/api.php
@@ -41,6 +41,45 @@ Route::get('/user/{id}', function ($id) {
     return apiUser::collection(User::where('status', 1)->where('id', $id)->get());
 });
 
+// #(http://localhost:8000/api/show_users)
+Route::get('/show_users','UserController@listUsers'); 
+
+// this api route storing the users information 
+//#(http://localhost:8000/api/store_user)
+Route::post('/store_user','UserController@insert_user');
+
+// this api route deleting the selected user 
+//#(http://localhost:8000/api/delete_user)
+Route::get('/delete_user/{id}','UserController@user_delete');
+
+// this api route reteriving the single user 
+//#(http://localhost:8000/api/get_user/user_id)
+Route::get('/get_user/{id}','UserController@user_get');
+
+// this api route update the selected user information 
+//#(http://localhost:8000/api/update_user/user_id)
+Route::post('/update_user/{id}','UserController@user_update');
+
+// #(http://localhost:8000/api/user/show_users)
+Route::get('/user/show_users','UserController@listUsers'); 
+
+// this api route storing the users information 
+//#(http://localhost:8000/api/user/store_user)
+Route::post('/user/store_user','UserController@insert_user');
+
+// this api route deleting the selected user 
+//#(http://localhost:8000/api/user/delete_user)
+Route::get('/user/delete_user/{id}','UserController@user_delete');
+
+// this api route reteriving the single user 
+//#(http://localhost:8000/api/user/get_user/user_id)
+Route::get('/user/get_user/{id}','UserController@user_get');
+
+// this api route update the selected user information 
+//#(http://localhost:8000/api/user/update_user/user_id)
+Route::post('/user/update_user/{id}','UserController@user_update');
+
+
 //All pages
 Route::get('/pages/{lang?}', function ($lang="en") {
     return apiPage::collection(Page::where('status', 1)->where('language', $lang)->paginate(32));
