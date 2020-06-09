5cfba37eb8fdace34760f126a3eeb38f55126d54
uyscuti-wiki <uyscuti.wiki@gmail.com>
2020-06-09 12:33:31 +0530

    import Auth and define $guard

--- app/Http/Middleware/LibraryManager.php
+++ app/Http/Middleware/LibraryManager.php
@@ -3,17 +3,19 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class LibraryManager
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     * @param string|null                     $guard
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
             if(!isLibraryManager()){
