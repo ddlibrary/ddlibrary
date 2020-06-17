6cfc2a3cef3450ec367e7a82fd8ba3a1de43a182
uyscuti-wiki <uyscuti.wiki@gmail.com>
2020-06-17 14:35:16 +0530

    add @property to PHPDoc
    # Conflicts:
    #	app/UserProfile.php

--- app/UserProfile.php
+++ app/UserProfile.php
@@ -4,6 +4,17 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property mixed      user_id
+ * @property mixed      first_name
+ * @property mixed      last_name
+ * @property mixed      country
+ * @property mixed|null city
+ * @property mixed      gender
+ * @property mixed      phone
+ * @method static find($userId)
+ * @method static where(string $string, $userId)
+ */
 class UserProfile extends Model
 {
     /**
