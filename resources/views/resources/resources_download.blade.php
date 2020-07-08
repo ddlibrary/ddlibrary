06693f82cd4b2ef77456c24f53240197d238488f
uyscuti-wiki <uyscuti.wiki@gmail.com>
2020-07-08 16:20:25 +0530

    new view for download progress

---
+++ resources/views/resources/resources_download.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main')
+@section('title')
+    @lang('Downloading attachment')
+@endsection
+@section('content')
+    @push('scripts')
+        <script
+        src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"
+        integrity="sha384-ZvpUoO/+PpLXR1lu4jmpXWu80pZlYUAfxl5NsBMWOEPSjUn/6Z/hRTt8+pR6L4N2"
+        crossorigin="anonymous"></script>
+    @endpush
+@endsection
