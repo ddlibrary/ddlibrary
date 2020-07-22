7a3cd5e8643a86c2da7944208725e3d835cfc33a
uyscuti-wiki <uyscuti.wiki@gmail.com>
2020-07-22 18:48:12 +0530

    update view blade for register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,26 +12,48 @@
 @section('content')
 <section class="ddl-forms">
     <header>
-        <h1>@lang('Register an Account with DDL')</h1>
+        <h1>@lang('Register an account with DDL')</h1>
     </header>
     <div class="content-body">
         @include('layouts.messages')
         <form method="POST" action="{{ route('register') }}">
         @csrf
         <div class="form-item">
-            <label for="email"> 
+            <label for="email">
                 <strong>@lang('Email')</strong>
-                <span class="form-required" title="This field is required.">*</span>
+                <span class="form-required" id="email-asterisk" title="This field is required." {{ $errors->has('phone')? 'style=display:none;' : '' }}>*</span>
+                <span id="email-preferred" style="display:none;">(preferred)</span>
             </label>
-            <input class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" id="email" name="email" size="40" type="email" value="{{ old('email') }}" required autofocus>
+            <input class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" id="email" name="email" size="40" type="email" value="{{ old('email') }}" {{ $errors->has('phone')? '' : 'required' }} autofocus>
             @if ($errors->has('email'))
                 <span class="invalid-feedback">
                     <strong>{{ $errors->first('email') }}</strong>
                 </span><br>
             @endif
             <div class="description">
-                @lang('A valid e-mail address. All e-mails from the system will be sent to this address. The e-mail address is not made public and will only be used if you wish to receive a new password or wish to receive certain news or notifications by e-mail.')
+                @lang(
+                    'Your email will be treated as confidential information and will be <br>used to
+                     reset your password and communicate to you. <br>If you do not own an email address,
+                     <a href="' . $gmail_signup_url . '" target="_blank">click here</a> to create one.'
+                     )
+                <br>
+                <span id="phone-text" {{ $errors->has('phone')? 'style=display:none;' : '' }}>
+                    @lang('Or, <span class="open-phone" id="phone-field" onclick="showDiv()">
+                     click here</span> to use your phone number to register.')
+                </span>
             </div>
+        </div>
+        <div class="form-item" id="phone-block" {{ $errors->has('phone')? '' : 'style=display:none;' }}>
+            <label for="phone">
+                <strong>@lang('Telephone number')</strong>
+                <span class="form-required" title="This field is required.">*</span>
+            </label>
+            <input class="form-control{{ $errors->has('phone') ? ' is-invalid' : '' }}" id="phone" name="phone" value="{{ old('phone') }}" size="40" type="tel" style="width: 327px;">
+            @if ($errors->has('phone'))
+                <span class="invalid-feedback">
+                    <strong>{{ $errors->first('phone') }}</strong>
+                </span>
+            @endif
         </div>
         <div class="form-item">
             <label for="username"> 
@@ -39,27 +61,46 @@
                 <span class="form-required" title="This field is required.">*</span>
             </label>
             <input class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" id="username" name="username" value="{{ old('username') }}" size="40" type="text" required>
-            <div class="description">
-                @lang('Spaces are allowed; punctuation is not allowed except for periods, hyphens, apostrophes, and underscores.')
-            </div>
-        </div>
-        <div class="form-item">
-            <label for="password"> 
-                <strong>@lang('Password')</strong>
-                <span class="form-required" title="This field is required.">*</span>
-            </label>
-            <input class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" id="password" name="password" size="40" type="password" required>
-            @if ($errors->has('password'))
+            @if ($errors->has('username'))
                 <span class="invalid-feedback">
-                    <strong>{{ $errors->first('password') }}</strong>
+                    <strong>{{ $errors->first('username') }}</strong>
                 </span>
             @endif
+            <div class="description">
+                @lang('Spaces are allowed; punctuation is not allowed except for periods, <br> hyphens, apostrophes, and underscores.')
+            </div>
         </div>
         <div class="personal-information">
             <div class="right-side">
                 <div class="form-item">
+                    <label for="password">
+                        <strong>@lang('Password')</strong>
+                        <span class="form-required" title="This field is required.">*</span>
+                    </label>
+                    <input class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" id="password" name="password" size="40" type="password" required>
+                    @if ($errors->has('password'))
+                        <span class="invalid-feedback">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                    @endif
+                    <div class="description">
+                        @lang('Choose a strong password with a minimum of eight characters, <br>combining at least one special character (!@#$%^&.) and a digit (0-9).')
+                    </div>
+                </div>
+            </div>
+            <div class="left-side">
+                <div class="form-item">
+                    <label for="password_confirmation">
+                        <strong>@lang('Confirm password')</strong>
+                        <span class="form-required" title="This field is required.">*</span>
+                    </label>
+                    <input class="form-control" id="password_confirmation" name="password_confirmation" size="40" type="password" required>
+                </div>
+            </div>
+            <div class="right-side">
+                <div class="form-item">
                     <label for="first_name"> 
-                        <strong>@lang('First Name')</strong>
+                        <strong>@lang('First name')</strong>
                         <span class="form-required" title="This field is required.">*</span>
                     </label>
                     <input class="form-control" id="first_name" name="first_name"  value="{{ old('first_name') }}" size="40" type="text" required>
@@ -68,7 +109,7 @@
             <div class="left-side">
                 <div class="form-item">
                     <label for="last_name"> 
-                        <strong>@lang('Last Name')</strong>
+                        <strong>@lang('Last name')</strong>
                         <span class="form-required" title="This field is required.">*</span>
                     </label>
                     <input class="form-control{{ $errors->has('last_name') ? ' is-invalid' : '' }}" id="last_name" name="last_name"  value="{{ old('last_name') }}" size="40" type="text" required>
@@ -76,24 +117,11 @@
             </div>
             <div class="right-side">
                 <div class="form-item">
-                    <label for="phone"> 
-                        <strong>@lang('Telephone Number')</strong>
-                    </label>
-                    <input class="form-control{{ $errors->has('age') ? ' is-invalid' : '' }}" id="phone" name="phone" value="{{ old('phone') }}" size="40" type="tel" style="width: 327px;">
-                    @if ($errors->has('phone'))
-                    <span class="invalid-feedback">
-                        <strong>{{ $errors->first('phone') }}</strong>
-                    </span>
-                    @endif
-                </div>
-            </div>
-            <div class="left-side">
-                <div class="form-item">
-                    <label for="gender"> 
+                    <label for="gender">
                         <strong>@lang('Gender')</strong>
                         <span class="form-required" title="This field is required.">*</span>
                     </label>
-                    <select class="form-control{{ $errors->has('gender') ? ' is-invalid' : '' }}" name="gender" id="gender" required>
+                    <select class="form-control{{ $errors->has('gender') ? ' is-invalid' : '' }}" style="width:338px;" {{-- 338px ~= 40pt (<input size=40>) --}} name="gender" id="gender" required>
                         <option value="">- @lang('None') -</option>
                         <option value="Male" {{ old('gender') == "Male" ? "selected" : "" }}>@lang('Male')</option>
                         <option value="Female" {{ old('gender') == "Female" ? "selected" : "" }}>@lang('Female')</option>
@@ -101,13 +129,14 @@
                     </select>
                 </div>
             </div>
+            <div class="left-side"></div>
             <div class="right-side">
                 <div class="form-item">
                     <label for="country"> 
                         <strong>@lang('Country')</strong>
                         <span class="form-required" title="This field is required.">*</span>
                     </label>
-                    <select class="form-control{{ $errors->has('country') ? ' is-invalid' : '' }}" name="country" id="country" onchange="javascript:populate(this,'city', {{ json_encode($provinces) }})" required>
+                    <select class="form-control{{ $errors->has('country') ? ' is-invalid' : '' }}" style="width:338px;" {{-- 338px ~= 40pt (<input size=40>) --}} name="country" id="country" onchange="javascript:populate(this,'city', {{ json_encode($provinces) }})" required>
                         <option value="">- @lang('None') -</option>
                         @foreach($countries AS $cn)
                         <option value="{{ $cn->tnid }}" {{ old('country') == $cn->tnid ? "selected" : "" }}>{{ $cn->name }}</option>
@@ -120,7 +149,7 @@
                     <label for="city"> 
                         <strong>@lang('City')</strong>
                     </label>
-                    <select class="form-control{{ $errors->has('city') ? ' is-invalid' : '' }}" name="city" id="city">
+                    <select class="form-control{{ $errors->has('city') ? ' is-invalid' : '' }}" style="width:338px;" {{-- 338px ~= 40pt (<input size=40>) --}} name="city" id="city">
                         <option value="">- @lang('None') -</option>
                     </select>
                     <input type="text" class="form-control" name="city_other" id="js-text-city" size="40" style="display:none;">
@@ -140,4 +169,17 @@
         </form>
     </div>
 </section>
+@push('scripts')
+    <script src="{{ asset('js/ddl.js') }}"></script>
+    <script>
+        function showDiv() {
+            document.getElementById('phone').required = true;
+            document.getElementById('email').required = false;
+            document.getElementById('email-asterisk').style.display = "none";
+            document.getElementById('phone-text').style.display = "none";
+            document.getElementById('phone-block').style.display = "block";
+            document.getElementById('email-preferred').style.display = "inline";
+        }
+    </script>
+@endpush
 @endsection
