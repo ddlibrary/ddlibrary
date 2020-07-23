c3baad78026a8578f730dd9e35ad349406dc406c
uyscuti-wiki <uyscuti.wiki@gmail.com>
2020-07-23 22:12:17 +0530

    update login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main')
 @section('title')
-@lang('Login with your DDL account') - @lang('Darakht-e Danesh Online Library')
+@lang('Log in to Darakht-e Danesh Library') - @lang('Darakht-e Danesh Library')
 @endsection
 @section('description')
 @lang('The Darakht-e Danesh Online Library for Educators is a repository of open educational resources for teachers, teacher trainers, school administrators, literacy workers and others involved in furthering education in Afghanistan.')
@@ -11,58 +11,49 @@
 @section('content')
 <section class="ddl-forms login">
     <header>
-        <h1>@lang('Login with your DDL account')</h1>
+        <h3>@lang('Log in to Darakht-e Danesh Library')</h3>
     </header>
     <div class="content-body">
         @include('layouts.messages')
         <form method="POST" action="{{ route('login') }}">
-        @csrf
-        <div class="form-item">
-            <label for="user-field"> 
-                <strong>@lang('Email or Username')</strong>
-                <span class="form-required" title="This field is required.">*</span>
-            </label>
-            <input class="form-control{{ $errors->has('user-field') ? ' is-invalid' : '' }}" id="user-field" name="user-field" size="40" type="text" value="{{ old('user-field') }}" required autofocus>
-            @if ($errors->has('user-field'))
-                <span class="invalid-feedback">
-                    <strong>{{ $errors->first('user-field') }}</strong>
-                </span><br>
-            @endif
-        </div>
+            @csrf
+            <div class="form-item">
+                <input class="form-control{{ $errors->has('user-field') ? ' is-invalid' : '' }}" id="user-field" name="user-field" placeholder="@lang('Email or username or phone')" size="40" type="text" value="{{ old('user-field') }}" required autofocus>
+                @if ($errors->has('user-field'))
+                    <span class="invalid-feedback">
+                        <strong>{{ $errors->first('user-field') }}</strong>
+                    </span><br>
+                @endif
+            </div>
 
-        <div class="form-item">
-            <label for="password"> 
-                <strong>@lang('Password')</strong>
-                <span class="form-required" title="This field is required.">*</span>
-            </label>
-            <input class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" id="password" name="password" size="40" type="password" required>
-            @if ($errors->has('password'))
-                <span class="invalid-feedback">
-                    <strong>{{ $errors->first('password') }}</strong>
-                </span>
-            @endif
-        </div>
-        <div class="form-item">
-            <label>
-                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
-            </label>
-        </div>
-
-        <div class="form-item">
-            <div class="left-side">
-                <input class="form-control normalButton" type="submit" value="@lang('Submit')">
+            <div class="form-item">
+                <input class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" id="password" name="password" placeholder="@lang('Password')" size="40" type="password" required>
+                @if ($errors->has('password'))
+                    <span class="invalid-feedback">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                @endif
             </div>
-            <div class="btn-div">
-                <a href="{{ route('password.request') }}">
-                    {{ __('Forgot Your Password?') }}
-                </a>
+            <div class="form-item">
+                <input class="form-control login-submit btn btn-primary" type="submit" value="@lang('Log in')">
             </div>
-            <div class="btn-div">
-                <a href="{{ route('register') }}">
-                    @lang('Register an Account with DDL')
-                </a>
+            <div class="form-item">
+                <label id="remember-me">
+                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember me') }}
+                </label>
             </div>
-        </div>
-    </form>
-</div>
+            <div class="form-item">
+                <div class="btn-div">
+                    <a href="{{ route('password.request') }}">
+                        {{ __('Forgot password?') }}
+                    </a>
+                    <span aria-hidden="true">&bull;</span>
+                    <a href="{{ route('register') }}">
+                        @lang('Register an account')
+                    </a>
+                </div>
+            </div>
+        </form>
+    </div>
+</section>
 @endsection
