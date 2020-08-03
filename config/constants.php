4b90b5e518cf7c8d69bc49103fe3a538f5a19515
uyscuti-wiki <uyscuti.wiki@gmail.com>
2020-08-03 19:15:34 +0530

    add s3 storage url

--- config/constants.php
+++ config/constants.php
@@ -2,6 +2,7 @@
 
 return [
 
-    'storyweaver_url' => 'https://ddl.storyweaver.org.in/api/v1/ThirdPartyUserReg'
+    'storyweaver_url' => 'https://ddl.storyweaver.org.in/api/v1/ThirdPartyUserReg',
+    'ddlmain_s3_file_storage_url' => 'https://ddl-resources.s3-ap-southeast-1.amazonaws.com'
 
 ];
