d8c577ddbb667bd38c21c41b60ac4dc0cf868aa2
Aaron Vasanth <uyscuti.wiki@gmail.com>
2020-10-15 19:40:09 +0530

    HOTFIX: StoryWeaver landing page URLs

--- config/constants.php
+++ config/constants.php
@@ -4,8 +4,8 @@ return [
 
     'storyweaver_url' => 'https://ddl.storyweaver.org.in/api/v1/ThirdPartyUserReg',
     'storyweaver_default' => 'https://ddl.storyweaver.org.in/',
-    'storyweaver_family_and_friends' => 'https://ddl.storyweaver.org.in/stories?category=Family%26Friends&query=&sort=Ratings',
-    'storyweaver_growing_up' => 'https://ddl.storyweaver.org.in/stories?category=GrowingUp&query=&sort=Ratings',
+    'storyweaver_family_and_friends' => 'https://ddl.storyweaver.org.in/stories?category=Family%20%26%20Friends&query=&sort=Ratings',
+    'storyweaver_growing_up' => 'https://ddl.storyweaver.org.in/stories?category=Growing%20Up&query=&sort=Ratings',
     'storyweaver_stem' => 'https://ddl.storyweaver.org.in/stories?category=STEM&query=&sort=Ratings',
     'storyweaver_funny' => 'https://ddl.storyweaver.org.in/stories?category=Funny&query=&sort=Ratings',
     'storyweaver_dari' => 'https://ddl.storyweaver.org.in/translate?target_language=Farsi',
