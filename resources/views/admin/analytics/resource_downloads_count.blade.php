f407a0d438e2687e90bdfb80a1ff0a4aea075373
Faisal Abubaker <ahmadfaisal47@gmail.com>
2021-03-10 08:44:07 +0430

    new report added for downloaded resources

---
+++ resources/views/admin/analytics/resource_downloads_count.blade.php
@@ -0,0 +1,29 @@
+@extends('admin.analytics.analytics_main')
+@section('analytics.content')
+<div class="row">
+    <div class="col-lg-12">
+        <!-- Example Bar Chart Card-->
+        <div class="card mb-3">
+            <div class="card-header">
+                <i class="fa fa-list"></i> Downloaded Resources Count
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                            <th>Total Downloads</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>{{ $downloadsCount }}</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
