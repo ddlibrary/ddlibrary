ee51158ca082ca9f66410bd3ca21d55e4d769694
uyscuti-wiki <uyscuti.wiki@gmail.com>
2021-04-13 01:44:32 +0530

    new model

---
+++ app/GlossarySubjects.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @method static orderBy(string $string, string $string1)
+ * @method static findOrFail(int $id)
+ * @property mixed en
+ * @property mixed fa
+ * @property mixed ps
+ */
+class GlossarySubjects extends Model
+{
+    //
+}
