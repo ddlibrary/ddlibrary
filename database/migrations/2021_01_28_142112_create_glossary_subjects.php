53d6b410095b1a4abf4dedf10f43b5ff3ade6f9e
Aaron Vasanth <uyscuti.wiki@gmail.com>
2021-04-15 15:25:29 +0530

    HOTFIX: let SQL know we need an InnoDB table

--- database/migrations/2021_01_28_142112_create_glossary_subjects.php
+++ database/migrations/2021_01_28_142112_create_glossary_subjects.php
@@ -14,6 +14,7 @@ class CreateGlossarySubjects extends Migration
     public function up()
     {
         Schema::create('glossary_subjects', function (Blueprint $table) {
+            $table->engine = “InnoDB”;
             $table->increments('id');
             $table->string('en', 100)->nullable()->default('');
             $table->string('fa', 100)->nullable()->default('');
