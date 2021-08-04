2510fd1f29bb7019b3454f6949163444b63e91ec
uyscuti-wiki <uyscuti.wiki@gmail.com>
2021-08-04 23:41:12 +0530

    laravel 5.8

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -6,7 +6,6 @@
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
       <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-      <a href="{{ URL::to('admin') }}" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> My Dashboard</a>
     </div>
 
     <!-- Content Row -->
@@ -214,6 +213,6 @@
   </div>
   <!-- /.container-fluid -->
 
-</div>
+
 <!-- End of Main Content -->
 @endsection
