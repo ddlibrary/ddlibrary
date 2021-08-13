6595ca0e2dc0ecaea32d4f060b874b3a135a14f0
uyscuti-wiki <uyscuti.wiki@gmail.com>
2021-08-13 20:58:37 +0530

    HOTFIX: update lfm config

--- config/lfm.php
+++ config/lfm.php
@@ -61,7 +61,7 @@ return [
             ],
         ],
         'image' => [
-            'folder_name'  => 'files',
+            'folder_name'  => '.',
             'startup_view' => 'grid',
             'max_size'     => 50000, // size in KB
             'valid_mime'   => [
