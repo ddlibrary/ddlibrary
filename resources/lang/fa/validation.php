982ec07c290758888163af3d1f4375cf0852048b
uyscuti-wiki <uyscuti.wiki@gmail.com>
2021-08-13 11:52:29 +0530

    HOTFIX: update lang files

---
+++ resources/lang/fa/validation.php
@@ -0,0 +1,16 @@
+<?php
+
+return array (
+  'custom' => 
+  array (
+    'attribute-name' => 
+    array (
+      'rule-name' => 'پیام تنظیم‌شده',
+    ),
+    'g-recaptcha-response' => 
+    array (
+      'captcha' => 'خطای کاپچه! لطفاً بعدتر امتحان کنید یا با مدیر سایت تماس بگیرید',
+      'required' => 'لطفاً تایید کنید که شما روابت نیستید',
+    ),
+  ),
+);
