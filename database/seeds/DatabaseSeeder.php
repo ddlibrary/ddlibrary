524d640ac183ab4d2b80ef8379451683977cb1b8
azizullahsaeidi <azizullahsaeidi@gmail.com>
2023-08-11 22:23:57 +0430

    Call new seeders in DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,9 +1,16 @@
 <?php
 
+use App\Http\Resources\Menu;
 use Database\Seeders\CitySeeder;
 use Database\Seeders\CountrySeeder;
+use Database\Seeders\EducationUseSeeder;
+use Database\Seeders\GlossarySubjectsSeeder;
+use Database\Seeders\LearningResourceTypeSeeder;
+use Database\Seeders\MenuSeeder;
+use Database\Seeders\ResourceLevelSeeder;
 use Database\Seeders\RoleSeeder;
 use Database\Seeders\SettingsSeeder;
+use Database\Seeders\SubjectAreaSeeder;
 use Database\Seeders\TaxonomyTermDataSeeder;
 use Illuminate\Database\Seeder;
 
@@ -18,9 +25,15 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(SettingsSeeder::class);
+        $this->call(MenuSeeder::class);
         $this->call(RoleSeeder::class);
         $this->call(TaxonomyTermDataSeeder::class);
         $this->call(CountrySeeder::class);
         $this->call(CitySeeder::class);
+        $this->call(GlossarySubjectsSeeder::class);
+        $this->call(SubjectAreaSeeder::class);
+        $this->call(LearningResourceTypeSeeder::class);
+        $this->call(EducationUseSeeder::class);
+        $this->call(ResourceLevelSeeder::class);
     }
 }
