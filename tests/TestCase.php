5276371d085595207a3e5da1d7fb635d29883063
azizullahsaeidi <azizullahsaeidi@gmail.com>
2023-08-30 20:56:00 +0430

    Add refreshApplicationWithLocale() and tearDown() in TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,22 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Mcamara\LaravelLocalization\LaravelLocalization;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    
+    protected function refreshApplicationWithLocale($locale)
+    {
+        self::tearDown();
+        putenv(LaravelLocalization::ENV_ROUTE_KEY . '=' . $locale);
+        self::setUp();
+    }
+
+    protected function tearDown(): void
+    {
+        putenv(LaravelLocalization::ENV_ROUTE_KEY);
+        parent::tearDown();
+    }
 }
