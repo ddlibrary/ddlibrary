378870b0e27673e7802cd270d464f9a5daa6ff61
azizullahsaeidi <azizullahsaeidi@gmail.com>
2023-08-30 20:55:08 +0430

    Create GlossaryListTest

---
+++ tests/Feature/Glossary/GlossaryListTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature\HomePage;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class GlossaryListTest extends TestCase
+{
+    
+    use RefreshDatabase, DatabaseMigrations;
+    
+    /** @test */
+    public function user_can_visit_glossary_page()
+    {
+        $this->refreshApplicationWithLocale('en');
+
+        $response = $this->get('/en/glossary');
+
+        $response->assertStatus(200)
+            ->assertViewIs('glossary.glossary_list');
+    }
+}
