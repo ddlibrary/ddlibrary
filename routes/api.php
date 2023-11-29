9c772fdead5dcf104458446fbfb5415a5a5a6de6
Shift <shift@laravelshift.com>
2023-11-29 13:08:15 +0000

    Adopt class based routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ApiController;
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,33 +16,33 @@
 
 // Protected Routes
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/user', 'ApiController@user');
-    Route::post('/logout', 'ApiController@logout');
-    Route::post('/favorites', 'ApiController@favorites');
+    Route::post('/user', [ApiController::class, 'user']);
+    Route::post('/logout', [ApiController::class, 'logout']);
+    Route::post('/favorites', [ApiController::class, 'favorites']);
 });
 
 // Login
-Route::post('/login', 'ApiController@login');
+Route::post('/login', [ApiController::class, 'login']);
 
 // Register
-Route::post('/register', 'ApiController@register');
+Route::post('/register', [ApiController::class, 'register']);
 
 // Pages route
-Route::get('/pages/{lang?}', 'ApiController@pages');
-Route::get('/page/{id}', 'ApiController@page');
-Route::get('/page_view/{id}', 'ApiController@pageView');
+Route::get('/pages/{lang?}', [ApiController::class, 'pages']);
+Route::get('/page/{id}', [ApiController::class, 'page']);
+Route::get('/page_view/{id}', [ApiController::class, 'pageView']);
 //News items
-Route::get('/news_list/{lang?}', 'ApiController@newsList');
-Route::get('/news/{id}', 'ApiController@news');
-Route::get('/news_view/{id}', 'ApiController@newsView');
+Route::get('/news_list/{lang?}', [ApiController::class, 'newsList']);
+Route::get('/news/{id}', [ApiController::class, 'news']);
+Route::get('/news_view/{id}', [ApiController::class, 'newsView']);
 // Useful Links
-Route::get('/links/{lang?}', 'ApiController@links');
+Route::get('/links/{lang?}', [ApiController::class, 'links']);
 // Resources
-Route::get('/resources/{lang?}', 'ApiController@resources');
-Route::get('/resource/{id}', 'ApiController@resource');
-Route::get('/resource_categories/{lang?}', 'ApiController@resourceCategories');
-Route::get('/resource_attributes/{id}', 'ApiController@resourceAttributes');
-Route::get('/resources/{lang}/{offset}', 'ApiController@resourceOffset');
-Route::get('/featured_resources/{lang?}', 'ApiController@featuredResources');
-Route::get('/filter_resources/{lang?}', 'ApiController@filterResources');
-Route::get('/resource/getFile/{fileId}', 'ApiController@getFile');
+Route::get('/resources/{lang?}', [ApiController::class, 'resources']);
+Route::get('/resource/{id}', [ApiController::class, 'resource']);
+Route::get('/resource_categories/{lang?}', [ApiController::class, 'resourceCategories']);
+Route::get('/resource_attributes/{id}', [ApiController::class, 'resourceAttributes']);
+Route::get('/resources/{lang}/{offset}', [ApiController::class, 'resourceOffset']);
+Route::get('/featured_resources/{lang?}', [ApiController::class, 'featuredResources']);
+Route::get('/filter_resources/{lang?}', [ApiController::class, 'filterResources']);
+Route::get('/resource/getFile/{fileId}', [ApiController::class, 'getFile']);
