b5dd4b95b54c10697add49c8b4b14878f39e3cf4
azizullahsaeidi <azizullahsaeidi@gmail.com>
2023-12-07 19:18:03 +0430

    Add email to database/migrations/2023_12_07_125201_create_subscribers_table.php

--- database/migrations/2023_12_07_125201_create_subscribers_table.php
+++ database/migrations/2023_12_07_125201_create_subscribers_table.php
@@ -15,6 +15,7 @@ class CreateSubscribersTable extends Migration
     {
         Schema::create('subscribers', function (Blueprint $table) {
             $table->id();
+            $table->string('email')->unique();
             $table->timestamps();
         });
     }
