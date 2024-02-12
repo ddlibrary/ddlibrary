a519c6d617af25a4f622825a0a4f0cd1294b7cdd
azizullahsaeidi <azizullahsaeidi@gmail.com>
2024-02-12 11:57:44 +0430

    Add recaptcha 3

--- config/services.php
+++ config/services.php
@@ -53,4 +53,10 @@ return [
         'redirect' => 'https://library.darakhtdanesh.org/login/google/callback/',
     ],
 
+    // Configure Google ReCAPTCHA v3 key+secret
+    'recaptcha_v3' => [
+        'siteKey' => env('RECAPTCHA_V3_SITE_KEY'),
+        'secretKey' => env('RECAPTCHA_V3_SECRET_KEY'),
+    ]
+
 ];
