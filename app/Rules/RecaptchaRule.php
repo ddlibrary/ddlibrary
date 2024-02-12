2ea05a30cc7924318dfdfa1e550980ee41f7269c
azizullahsaeidi <azizullahsaeidi@gmail.com>
2024-02-12 14:02:29 +0430

    Create RecaptchaRule

---
+++ app/Rules/RecaptchaRule.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Support\Facades\Http;
+
+class RecaptchaRule implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        $googleResponse = Http::asForm()->post('https://www.google.com/recaptcha/api/siteverify', [
+            'secret' => config('services.recaptcha_v3.secret_key'),
+            'response' => $value,
+            'remoteip' => request()->id,
+        ]);
+
+        if(!$googleResponse->json('success')){
+
+            $fail("The {$attribute} is invalid.");
+        }
+    }
+}
