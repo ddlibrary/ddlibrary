44b437788dea77408c59d8c3625731d511cc8fc2
azizullahsaeidi <azizullahsaeidi@gmail.com>
2024-02-16 07:14:05 +0430

    Add index and store functions for subscribers

--- app/Http/Controllers/SubscribeController.php
+++ app/Http/Controllers/SubscribeController.php
@@ -2,20 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SubscribeRequest;
 use App\Models\Subscriber;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
 
 class SubscribeController extends Controller
 {
-    public function subscribe(Request $request): RedirectResponse
+    public function index(): View
     {
-        $request->validate([
-            'email' => 'required|email|unique:subscribers,email',
-            'name' => 'required|string'
-        ]);
+        return view('subscribe.index');
+    }
 
+    public function store(SubscribeRequest $request): RedirectResponse
+    {
         Subscriber::create([
             'email' => $request->email,
             'name' => $request->name,
