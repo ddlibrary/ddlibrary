617558d8e81a45be6aa6a6dc68d639b1f0059127
azizullahsaeidi <azizullahsaeidi@gmail.com>
2024-02-22 21:39:11 +0430

    Change search btn to search icon

--- resources/views/layouts/search.blade.php
+++ resources/views/layouts/search.blade.php
@@ -1,12 +1,25 @@
 <section class="banner">
-    <header>
-        <h1>@lang('Free and open educational resources for Afghanistan')</h1>
-    </header>
-    <form method="GET" action="{{ route('resourceList') }}" id="search-form">
-        @csrf
-        <label for="search">
-            <input type="search" name="search" class="form-search form-control" value="{{ session('search') }}" placeholder="@lang('SEARCH OUR GROWING LIBRARY!')" autofocus>
-        </label>
-        <input type="submit" class="search-button" value="@lang('Go')">
-    </form>
+    <div class="main-section" style="background:#eee; border:1px dotted gray;padding:40px 0px;">
+
+        <header>
+            <h1>@lang('Free and open educational resources for Afghanistan')</h1>
+        </header>
+        <form method="GET" action="{{ route('resourceList') }}" id="search-form">
+            @csrf
+
+            <div class="relative-div">
+                <input type="search" name="search" class="form-control search-input" value="{{ session('search') }}"
+                    placeholder="@lang('SEARCH OUR GROWING LIBRARY!')" autofocus>
+                <i class="fa fa-search fa-2x search-icon {{ Lang::locale() == 'en' ? 'search-icon-right' : 'search-icon-left'}}" onclick="submitForm()"></i>
+                {{-- <input type="submit" class="search-button" value="@lang('Go')"> --}}
+            </div>
+        </form>
+    </div>
+    @push('scripts')
+        <script>
+            function submitForm() {
+                document.getElementById('search-form').submit();
+            }
+        </script>
+    @endpush
 </section>
