e313c913888d4675818c0188d535d10cec161511
azizullahsaeidi <azizullahsaeidi@gmail.com>
2024-02-22 22:07:15 +0430

    Add alert message

--- resources/views/layouts/search.blade.php
+++ resources/views/layouts/search.blade.php
@@ -1,6 +1,8 @@
 <section class="banner">
     <div class="main-section" style="background:#eee; border:1px dotted gray;padding:40px 0px;">
-
+        @if (session()->has('alert'))
+            <x-alert :message="Session::get('alert.message')" :level="Session::get('alert.level')" />
+        @endif
         <header>
             <h1>@lang('Free and open educational resources for Afghanistan')</h1>
         </header>
@@ -10,7 +12,8 @@
             <div class="relative-div">
                 <input type="search" name="search" class="form-control search-input" value="{{ session('search') }}"
                     placeholder="@lang('SEARCH OUR GROWING LIBRARY!')" autofocus>
-                <i class="fa fa-search fa-2x search-icon {{ Lang::locale() == 'en' ? 'search-icon-right' : 'search-icon-left'}}" onclick="submitForm()"></i>
+                <i class="fa fa-search fa-2x search-icon {{ Lang::locale() == 'en' ? 'search-icon-right' : 'search-icon-left' }}"
+                    onclick="submitForm()"></i>
                 {{-- <input type="submit" class="search-button" value="@lang('Go')"> --}}
             </div>
         </form>
