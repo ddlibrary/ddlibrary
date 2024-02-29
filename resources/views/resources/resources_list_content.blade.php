00118ae10e38ac9c5155bef3daa0b55c804243de
azizullahsaeidi <azizullahsaeidi@gmail.com>
2024-02-29 16:44:36 +0430

    Change book container to grid

--- resources/views/resources/resources_list_content.blade.php
+++ resources/views/resources/resources_list_content.blade.php
@@ -1,39 +1,42 @@
 @if ($resources)
-    @foreach ($resources->unique('id') as $resource)
-        @if ($resource->status)
-            <article class="resource-article resource-information">
-                <div class="resource-items">
-                    <div class="p-1">
-                        <a href="{{ URL::to('resource/' . $resource->id) }}" target="_blank" class="image-link">
-                            <div class="overflow-hidden">
-                                <img class="resource-img lazyload"
-                                    data-src="{{ getImagefromResource($resource->abstract) }}" alt="Resource Image">
-                            </div>
-                        </a>
+    <div class="w-100 book-list">
+
+        @foreach ($resources->unique('id') as $resource)
+            @if ($resource->status)
+                <article class="">
+                    <div class="resource-items">
+                        <div class="p-1 w-100">
+                            <a href="{{ URL::to('resource/' . $resource->id) }}" target="_blank" class="image-link">
+                                <div class="overflow-hidden w-100">
+                                    <img class="w-100 book-cover lazyload"
+                                        data-src="{{ getImagefromResource($resource->abstract) }}" alt="Resource Image">
+                                </div>
+                            </a>
+                        </div>
+                        <div class="resource-title flex-1 w-100">
+                            <a href="{{ URL::to('resource/' . $resource->id) }}" target="_blank" class="resource-link">
+                                {{ $resource->title }}
+                            </a>
+                        </div>
+                        <div class="resource-details w-100">
+                            <article>
+                                <i
+                                    class="fas fa-eye"></i><span>{{ $views->where('resource_id', $resource->id)->count() }}</span>
+                            </article>
+                            <article>
+                                <i
+                                    class="fas fa-star"></i><span>{{ $favorites->where('resource_id', $resource->id)->count() }}</span>
+                            </article>
+                            <article>
+                                <i
+                                    class="fas fa-comment"></i><span>{{ $comments->where('resource_id', $resource->id)->count() }}</span>
+                            </article>
+                        </div>
                     </div>
-                    <div class="resource-title flex-1">
-                        <a href="{{ URL::to('resource/' . $resource->id) }}" target="_blank" class="resource-link">
-                            {{ $resource->title }}
-                        </a>
-                    </div>
-                    <div class="resource-details">
-                        <article>
-                            <i
-                                class="fas fa-eye"></i><span>{{ $views->where('resource_id', $resource->id)->count() }}</span>
-                        </article>
-                        <article>
-                            <i
-                                class="fas fa-star"></i><span>{{ $favorites->where('resource_id', $resource->id)->count() }}</span>
-                        </article>
-                        <article>
-                            <i
-                                class="fas fa-comment"></i><span>{{ $comments->where('resource_id', $resource->id)->count() }}</span>
-                        </article>
-                    </div>
-                </div>
-            </article>
-        @endif
-    @endforeach
+                </article>
+            @endif
+        @endforeach
+    </div>
 @else
     <h2>@lang('No records found!')</h2>
 @endif
