30de7740a46996bc2a4a2008050d3868308a7080
azizullahsaeidi <azizullahsaeidi@gmail.com>
2024-03-07 15:11:42 +0430

    Add alert component to subscriber.index

--- resources/views/subscribe/index.blade.php
+++ resources/views/subscribe/index.blade.php
@@ -10,78 +10,84 @@
     {{ asset('storage/files/logo-dd.png') }}
 @endsection
 @section('content')
-    <style>
-        .form-item .grecaptcha-badge {
-            display: absolute;
-        }
-    </style>
-    <section class="p-20 ddl-forms login">
+@if (session()->has('alert'))
+    <x-alert :message="Session::get('alert.message')" :level="Session::get('alert.level')" />
+@endif
+
+    <div class="display-flex justify-content-center">
         <div>
-            @if (!$subscriber)
-                <header>
-                    <h2>@lang('Subscribe to our newsletter')</h2>
-                    <h4>@lang("You'll receive our monthly newsletter with our latest updates, highlights, top resources, educational content and other relevant news.")</h4>
-                </header>
+            <header>
+                <h2 class="text-center">@lang('Subscribe to our newsletter')</h2>
+                <p class="text-center">@lang("You'll receive our monthly newsletter with our latest updates, highlights, top resources, educational content and other relevant news.")</p>
+            </header>
+            <section class="p-8 d-block ddl-forms register-form">
                 <div>
-                    <form method="POST" action="{{ url('subscribe') }}" id="subscribe-form">
-                        @csrf
-                        @honeypot
+                    @if (!$subscriber)
+                        <div>
+                            <form method="POST" action="{{ route('subscribe.store') }}" id="subscribe-form">
+                                @csrf
+                                @honeypot
 
-                        {{-- Name --}}
-                        <div class="form-item">
-                            <input type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}"
-                                id="name" name="name" autocomplete="username" spellcheck="false"
-                                placeholder="@lang('Please enter your name')" size="40"
-                                value="{{ auth()->check() ? auth()->user()->username : '' }}" autofocus required>
-                            @if ($errors->has('name'))
-                                <span class="invalid-feedback text-start">
-                                    <span>{{ $errors->first('name') }}</span>
-                                </span>
-                            @endif
+                                {{-- Name --}}
+                                <div class="form-item">
+                                    <input type="text"
+                                        class="form-control w-100 {{ $errors->has('name') ? ' is-invalid' : '' }}"
+                                        id="name" name="name" autocomplete="username" spellcheck="false"
+                                        placeholder="@lang('Please enter your name')" size="40"
+                                        value="{{ auth()->check() ? auth()->user()->username : '' }}" autofocus>
+                                    @if ($errors->has('name'))
+                                        <span class="invalid-feedback text-start">
+                                            <span>{{ $errors->first('name') }}</span>
+                                        </span>
+                                    @endif
+                                </div>
+
+                                {{-- Email --}}
+                                <div class="form-item">
+                                    <input type="text"
+                                        class="form-control w-100 {{ $errors->has('email') ? ' is-invalid' : '' }}"
+                                        id="email" name="email" autocomplete="email" spellcheck="false"
+                                        placeholder="@lang('Please enter your email')" size="40"
+                                        value="{{ auth()->check() ? auth()->user()->email : '' }}" autofocus>
+                                    @if ($errors->has('email'))
+                                        <span class="invalid-feedback text-start">
+                                            <span>{{ $errors->first('email') }}</span>
+                                        </span>
+                                    @endif
+                                </div>
+
+                                {{-- Submit Button --}}
+                                <div class="form-item">
+                                    <input type="submit" value="@lang('Subscribe')"
+                                        class="g-recaptcha form-control login-submit btn btn-primary w-100"
+                                        data-sitekey="{{ config('services.recaptcha_v3.site_key') }}"
+                                        data-callback='onSubmit' data-action='subscribe'>
+
+                                </div>
+
+                                {{-- Mailchimp --}}
+                                <small style="color:gray">
+                                    @lang('Your email will be shared with MailChimp. their privacy policy')
+                                </small>
+
+                            </form>
                         </div>
-
-                        {{-- Email --}}
-                        <div class="form-item">
-                            <input type="email"
-                                class="form-control w-100 {{ $errors->has('email') ? ' is-invalid' : '' }}" id="email"
-                                name="email" autocomplete="email" spellcheck="false" placeholder="@lang('Please enter your email')"
-                                size="40" value="{{ auth()->check() ? auth()->user()->email : '' }}" required>
-                            @if ($errors->has('email'))
-                                <span class="invalid-feedback text-start">
-                                    <span>{{ $errors->first('email') }}</span>
-                                </span>
-                            @endif
-                        </div>
-
-                        {{-- Submit Button --}}
-                        <div class="form-item">
-                            <input type="submit" value="@lang('Subscribe')"
-                                class="g-recaptcha form-control login-submit btn btn-primary w-100"
-                                data-sitekey="{{ config('services.recaptcha_v3.site_key') }}" data-callback='onSubmit'
-                                data-action='subscribe'>
-
-                        </div>
-
-                        {{-- Mailchimp --}}
-                        <p style="color:gray; font-size: 12px;">
-                            @lang('Your email will be shared with MailChimp.')
-                        </p>
-
-                    </form>
+                    @else
+                        {{-- Already Subscribed --}}
+                        <header>
+                            <h2 class="text-center">@lang('Thank you')</h2>
+                            <p class="text-center">@lang('You subscribed our newsletter.')</p>
+                            <p class="text-center">
+                                <br>
+                                <a href="{{ url('/') }}">@lang('Home') <i class="fas fa-home fa-lg icons"></i></a>
+                            </p>
+                        </header>
+                    @endif
                 </div>
-            @else
-                {{-- Already Subscribed --}}
-                <header>
-                    <h2>@lang('Thank you')</h2>
-                    <h3>@lang('You already subscribed our newsletter.')</h3>
-                    <h3>
-                        <br>
-                        <a href="{{ url('/') }}">@lang('Home') <i class="fas fa-home fa-lg icons"></i></a>
-                    </h3>
-                </header>
-            @endif
+            </section>
         </div>
-    </section>
+    </div>
+
     @push('scripts')
         <script>
             function onSubmit(token) {
@@ -89,4 +95,5 @@
             }
         </script>
     @endpush
+
 @endsection
