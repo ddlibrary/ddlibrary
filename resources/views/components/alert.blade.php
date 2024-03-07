34481d5b49584a04f71052f19bee4db01a0ad1ae
azizullahsaeidi <azizullahsaeidi@gmail.com>
2024-03-07 14:56:26 +0430

    Create alert component

---
+++ resources/views/components/alert.blade.php
@@ -0,0 +1,17 @@
+<div style="position: absolute; display: none;" id="alert-message">
+    <div class="alert alert-{{ $level }}">
+        <p>
+            {{ $message }}
+        <div class="progress-bar progress-bar-{{$level}}"></div>
+        </p>
+    </div>
+</div>
+<script>
+    window.onload = function() {
+        var successMessageElement = document.getElementById('alert-message');
+        successMessageElement.style.display = 'block';
+        setTimeout(function() {
+            successMessageElement.style.display = 'none';
+        }, 10000);
+    };
+</script>
