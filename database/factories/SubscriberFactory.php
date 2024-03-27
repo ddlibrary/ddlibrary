84d611b7d32a6b06e9bb3fa31a65ac3d589364ec
azizullahsaeidi <azizullahsaeidi@gmail.com>
2024-03-27 20:55:11 +0430

    Add user_id on SubscriberFactory

--- database/factories/SubscriberFactory.php
+++ database/factories/SubscriberFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -18,7 +19,8 @@ class SubscriberFactory extends Factory
     {
         return [
             'name' => $this->faker->userName(),
-            'email' => $this->faker->unique()->safeEmail()
+            'email' => $this->faker->unique()->safeEmail(),
+            'user_id' => User::factory()->create(),
         ];
     }
 }
