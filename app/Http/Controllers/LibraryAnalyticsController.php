560d7e22b10473043c22d0738dd52d7625ea7eb8
Azizullah Saeidi <azizullahsaeidi@gmail.com>
2024-04-24 23:02:37 +0430

    Add totalResources

--- app/Http/Controllers/LibraryAnalyticsController.php
+++ app/Http/Controllers/LibraryAnalyticsController.php
@@ -2,13 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\DownloadCount;
+use App\Models\Resource;
+use App\Models\TaxonomyTerm;
+use App\Traits\GenderTrait;
+use App\Traits\LanguageTrait;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
 
 class LibraryAnalyticsController extends Controller
 {
-    public function index() : View {
-        
-        return view('admin.library-analytics.index');
+    use LanguageTrait, GenderTrait;
+
+    public function index(Request $request): View
+    {
+
+
+        $languages = $this->getLanguages();
+
+        $genders = $this->genders();
+
+        $totalResources = Resource::groupBy('language')->select('language', DB::raw('count(*) as count'))->get();
+
+        return view('admin.library-analytics.index', compact(['records', 'genders', 'languages', 'reportType', 'totalResources']));
     }
 }
