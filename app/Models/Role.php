6472311b0ef09c6ece25e5fe2d02acbcc3aeb988
Azizullah Saeidi <azizullahsaeidi@gmail.com>
2024-04-29 11:42:40 +0430

    Add user() on Role model

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    public function users(){
+        return $this->belongsToMany(User::class, 'user_roles');
+    }
 }
