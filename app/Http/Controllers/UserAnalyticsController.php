03f5fcda2d4524603b92bfab3b43cf59348efcef
Azizullah Saeidi <azizullahsaeidi@gmail.com>
2024-04-30 20:25:25 +0430

    Add getTop10ActiveUsers()

--- app/Http/Controllers/UserAnalyticsController.php
+++ app/Http/Controllers/UserAnalyticsController.php
@@ -13,6 +13,7 @@ class UserAnalyticsController extends Controller
 {
     public function index(Request $request): View
     {
+        $top10ActiveUsers = $this->getTop10ActiveUsers($request);
         $roles = $this->getTotalUsersBaseOnRole($request);
         $totalRegisteredUsers = $this->getTotalRegisteredUsers($request);
         $totalUsersBaseOnGenders = $this->getTotalUsersBaseOnGender($request);
@@ -20,7 +21,7 @@ class UserAnalyticsController extends Controller
         $totalGoogleUsers = $this->getTotalUsersBaseOnRegistrationSource($request, 'google');
         $totalFacebookUsers = $this->getTotalUsersBaseOnRegistrationSource($request, 'facebook');
 
-        return view('admin.analytics.users.index', compact(['roles', 'totalUsersBaseOnGenders', 'totalRegisteredUsers', 'totalUsers', 'totalGoogleUsers', 'totalFacebookUsers']));
+        return view('admin.analytics.users.index', compact(['roles', 'totalUsersBaseOnGenders', 'totalRegisteredUsers', 'totalUsers', 'totalGoogleUsers', 'totalFacebookUsers', 'top10ActiveUsers']));
     }
 
     private function getTotalUsersBaseOnRole($request): Collection
@@ -70,5 +71,25 @@ class UserAnalyticsController extends Controller
             });
 
         return $query->count();
+    }
+
+    private function getTop10ActiveUsers($request){
+        $query = DB::table("activity_log")
+        ->select(
+            "user_profiles.first_name",
+            "user_profiles.last_name",
+            DB::raw("count(*) as activity_count")
+        )
+        ->join("users", "users.id", "=", "activity_log.causer_id")
+        ->join("user_profiles", "user_profiles.user_id", "=", "users.id")
+        ->groupBy("users.id", "user_profiles.last_name", "user_profiles.first_name")
+        ->orderByDesc("activity_count")
+        ->limit(10);
+
+        if($request->date_from && $request->date_to) {
+            $query->whereBetween('activity_log.created_at', [$request->date_from, $request->date_to]);
+        };
+
+        return $query->get();
     }
 }
