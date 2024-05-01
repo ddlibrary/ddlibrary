2d1a1aceca466e24d0a6590b2dac06f78ce00e5e
Azizullah Saeidi <azizullahsaeidi@gmail.com>
2024-05-01 18:59:42 +0430

    Add language dropdown

--- resources/views/admin/analytics/sitewide/index.blade.php
+++ resources/views/admin/analytics/sitewide/index.blade.php
@@ -14,6 +14,15 @@
                             <label>To Date</label>
                             <input type="date" value="{{ request()->date_to }}" class="form-control" name="date_to">
                         </div>
+                        <div class="col-md-2">
+                            <label>Language</label>
+                            <select class="form-control" name="language">
+                                <option value="">...</option>
+                                @foreach($languages as $locale => $properties)
+                                    <option value="{{ $locale }}" @selected($locale==request()->language)>{{ $properties['name'] }}</option>
+                                @endforeach
+                            </select>
+                        </div>
 
                         <div class="col-md-2" style="align-self: flex-end">
                             <input class="btn btn-primary" type="submit" value="Filter">
@@ -53,6 +62,47 @@
                                             <div class="p-1">
                                                 <span class="badge badge-info">
                                                     {{ number_format($top10ViewedResources->sum('views_count')) }}
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        {{-- Total views base on user types  --}}
+                        <div class="col-sm-6 col-md-4 col-lg-3">
+                            <div class="card border-secondary mb-3">
+                                <div class="card-header">Total views base on user types </div>
+                                <div class="card-body text-secondary p-2">
+                                    <div class="card-text">
+                                        <div class="d-flex justify-content-between mb-2 rounded bg-light text-dark">
+                                            <div class="p-1 text-capitalize">
+                                                1. Total View
+                                            </div>
+                                            <div class="p-1">
+                                                <span class="badge badge-info">
+                                                    {{ number_format($totalViews) }}
+                                                </span>
+                                            </div>
+                                        </div>
+                                        <div class="d-flex justify-content-between mb-2 rounded bg-light text-dark">
+                                            <div class="p-1 text-capitalize">
+                                                2. Unregistered users views
+                                            </div>
+                                            <div class="p-1">
+                                                <span class="badge badge-info">
+                                                    {{ number_format($totalGuestViews) }}
+                                                </span>
+                                            </div>
+                                        </div>
+                                        <div class="d-flex justify-content-between mb-2 rounded bg-light text-dark">
+                                            <div class="p-1 text-capitalize">
+                                                3. Registered users views
+                                            </div>
+                                            <div class="p-1">
+                                                <span class="badge badge-info">
+                                                    {{ number_format($totalRegisteredUsersViews) }}
                                                 </span>
                                             </div>
                                         </div>
