88a26d653ed3fe25727c4a1e81d1487da0eaf6ed
Azizullah Saeidi <azizullahsaeidi@gmail.com>
2024-05-01 14:22:44 +0430

    Add index on resource_id and created_at in resource_views table

---
+++ database/migrations/2024_05_01_094648_add_index_to_resource_id_and_created_at_on_resource_views_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('resource_views', function (Blueprint $table) {
+            $table->index(['resource_id', 'created_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('resource_views', function (Blueprint $table) {
+            $table->dropIndex(['resource_id', 'created_at']);
+        });
+    }
+};
