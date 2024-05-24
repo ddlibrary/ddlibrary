4f598bd2b36e8c86995b6cbdf6e98f82344f9c6e
Azizullah Saeidi <azizullahsaeidi@gmail.com>
2024-05-24 11:21:55 +0430

    Update LanguageEnum.php

--- app/Enums/LanguageEnum.php
+++ app/Enums/LanguageEnum.php
@@ -5,7 +5,7 @@ namespace App\Enums;
 enum LanguageEnum: string
 {
     case Farsi = 'fa';
-    case sowji = 'sw';
+    case Sowji = 'sw';
     case Munji = 'mj';
     case Pashto = 'ps';
     case Uzbaki = 'uz';
