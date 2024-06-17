e6526459886fdef4f4a930db231f77740d5ff864
Aaron <uyscuti.wiki@gmail.com>
2024-06-17 12:53:58 +0530

    [HOTFIX] exclude elevated roles from active users

--- app/Http/Controllers/UserAnalyticsController.php
+++ app/Http/Controllers/UserAnalyticsController.php
@@ -74,7 +74,7 @@ class UserAnalyticsController extends Controller
 
     private function getTop10ActiveUsers($request): object
     {
-        $query = DB::table('activity_log')->select('user_profiles.first_name', 'user_profiles.last_name', DB::raw('count(*) as activity_count'))->join('users', 'users.id', '=', 'activity_log.causer_id')->join('user_profiles', 'user_profiles.user_id', '=', 'users.id')->groupBy('users.id', 'user_profiles.last_name', 'user_profiles.first_name')->orderByDesc('activity_count')->limit(10);
+        $query = DB::table('activity_log')->select('user_profiles.first_name', 'user_profiles.last_name', DB::raw('count(*) as activity_count'))->join('users', 'users.id', '=', 'activity_log.causer_id')->join('user_profiles', 'user_profiles.user_id', '=', 'users.id')->groupBy('users.id', 'user_profiles.last_name', 'user_profiles.first_name')->whereNotIn('id', [UserRoleEnum::SiteAdministrator, UserRoleEnum::LibraryManager, UserRoleEnum::LibraryTeacher])->orderByDesc('activity_count')->limit(10);
 
         if ($request->date_from && $request->date_to) {
             $query->whereBetween('activity_log.created_at', [$request->date_from, $request->date_to]);
