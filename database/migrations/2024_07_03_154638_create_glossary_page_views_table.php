9d91affb938fdb1f5f78f1c457762a2f1c64552e
Azizullah Saeidi <azizullahsaeidi@gmail.com>
2024-07-03 23:20:32 +0430

    Remove url

--- database/migrations/2024_07_03_154638_create_glossary_page_views_table.php
+++ database/migrations/2024_07_03_154638_create_glossary_page_views_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,29 +12,23 @@ return new class extends Migration
     {
         Schema::create('glossary_page_views', function (Blueprint $table) {
             $table->id();
-            $table->string('page_url', 255);
             $table->string('user_agent', 255);
             $table->string('ip_address', 45);
             $table->string('browser', 45);
             $table->string('title')->nullable();
-            
+
             $table->boolean('is_bot')->default(false);
             $table->string('language', 4);
             $table->string('gender', 11)->index()->nullable();
             $table->foreignId('device_id')->constrained('devices');
             $table->foreignId('platform_id')->constrained('platforms');
             $table->foreignId('browser_id')->constrained('browsers');
-            $table->tinyInteger('status')->comment("1: view, 2: created");
+            $table->tinyInteger('status')->comment('1: view, 2: create');
             $table->unsignedInteger('user_id')->nullable();
-            $table->unsignedInteger('glossary_id');
+            $table->unsignedInteger('glossary_subject_id')->nullable();
 
-            $table->foreign('user_id')
-            ->references('id')
-            ->on('users');
-
-            $table->foreign('glossary_id')
-            ->references('id')
-            ->on('glossary');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('glossary_subject_id')->references('id')->on('glossary_subjects');
 
             $table->timestamps();
         });
