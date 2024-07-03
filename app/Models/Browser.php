b6a68253290c7fd8642302ae24f56dff22e6257c
Azizullah Saeidi <azizullahsaeidi@gmail.com>
2024-07-03 23:21:55 +0430

    Add new relationships trait

--- app/Models/Browser.php
+++ app/Models/Browser.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\Relations\HasManyGlossaryPageView;
 use App\Models\Relations\HasManySitewidesPageView;
 use Illuminate\Database\Eloquent\Model;
 
 class Browser extends Model
 {
-    use HasManySitewidesPageView;
+    use HasManySitewidesPageView, HasManyGlossaryPageView;
 
     protected $fillable = ['name'];
 }
