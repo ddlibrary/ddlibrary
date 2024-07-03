c60305dbe7c3e28ff46b1e746b033fa828e92679
Azizullah Saeidi <azizullahsaeidi@gmail.com>
2024-07-03 23:23:15 +0430

    Add new relationships

--- app/Models/GlossaryPageView.php
+++ app/Models/GlossaryPageView.php
@@ -2,10 +2,36 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Relations\BelongsToUser;
 use Illuminate\Database\Eloquent\Model;
 
 class GlossaryPageView extends Model
 {
-    use HasFactory;
+    use BelongsToUser;
+
+    protected $guarded = [];
+
+    protected $casts = [
+        'is_bot' => 'boolean',
+    ];
+
+    public function glossarySubject()
+    {
+        return $this->belongsTo(GlossarySubject::class);
+    }
+
+    public function platform()
+    {
+        return $this->belongsTo(Platform::class);
+    }
+
+    public function browser()
+    {
+        return $this->belongsTo(Browser::class);
+    }
+
+    public function device()
+    {
+        return $this->belongsTo(Device::class);
+    }
 }
