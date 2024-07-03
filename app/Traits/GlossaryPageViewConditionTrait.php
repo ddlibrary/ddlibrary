6452ae5e8864ba5ef8b42ecb79645606ffe92af4
Azizullah Saeidi <azizullahsaeidi@gmail.com>
2024-07-03 23:17:36 +0430

    Create GlossaryPageViewConditionTrait

---
+++ app/Traits/GlossaryPageViewConditionTrait.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Traits;
+
+trait GlossaryPageViewConditionTrait
+{
+    protected function filterPageViews($query, $request)
+    {
+        if ($request->platform_id) {
+            $query->where('platform_id', $request->platform_id);
+        }
+        if ($request->gender) {
+            $query->where('gender', $request->gender);
+        }
+        if ($request->device_id) {
+            $query->where('device_id', $request->device_id);
+        }
+        if ($request->glossary_subject_id) {
+            $query->where('glossary_subject_id', $request->glossary_subject_id);
+        }
+        if ($request->language) {
+            $query->where('language', $request->language);
+        }
+        if ($request->status) {
+            $query->where('status', $request->status);
+        }
+        if ($request->browser_id) {
+            $query->where('browser_id', $request->browser_id);
+        }
+
+        if ($request->is_bot) {
+            $query->where('is_bot', $request->is_bot ==1 ? true : false);
+        }
+        if ($request->date_from && $request->date_to) {
+            $query->whereBetween('created_at', [$request->date_from . ' 00:00:00', $request->date_to . ' 23:59:59']);
+        }
+
+        return $query;
+    }
+}
