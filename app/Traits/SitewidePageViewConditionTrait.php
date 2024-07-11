07cf57c4acf5960e8ff3ac0b65cb18342970a19f
Azizullah Saeidi <azizullahsaeidi@gmail.com>
2024-07-11 17:22:15 +0430

    Remove page_type_id from SitewidePageViewConditionTrait

--- app/Traits/SitewidePageViewConditionTrait.php
+++ app/Traits/SitewidePageViewConditionTrait.php
@@ -9,24 +9,27 @@ trait SitewidePageViewConditionTrait
         if ($request->platform_id) {
             $query->where('platform_id', $request->platform_id);
         }
+
         if ($request->gender) {
             $query->where('gender', $request->gender);
         }
+
         if ($request->device_id) {
             $query->where('device_id', $request->device_id);
         }
+
         if ($request->page_url) {
             $query->where('page_url', url('').'/'.$request->page_url);
         }
+
         if ($request->glossary_subject_id) {
             $query->where('glossary_subject_id', $request->glossary_subject_id);
         }
+
         if ($request->language) {
             $query->where('language', $request->language);
         }
-        if ($request->page_type_id) {
-            $query->where('page_type_id', $request->page_type_id);
-        }
+
         if ($request->browser_id) {
             $query->where('browser_id', $request->browser_id);
         }
@@ -34,6 +37,7 @@ trait SitewidePageViewConditionTrait
         if ($request->is_bot) {
             $query->where('is_bot', $request->is_bot ==1 ? true : false);
         }
+        
         if ($request->date_from && $request->date_to) {
             $query->whereBetween('created_at', [$request->date_from . ' 00:00:00', $request->date_to . ' 23:59:59']);
         }
