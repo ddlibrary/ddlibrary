872500fbb74cf11086949e08adc86f8dec8f8277
Azizullah Saeidi <azizullahsaeidi@gmail.com>
2024-07-11 17:25:13 +0430

    Remove ip from glossary_page_views migration

--- database/migrations/2024_07_03_154638_create_glossary_page_views_table.php
+++ database/migrations/2024_07_03_154638_create_glossary_page_views_table.php
@@ -13,7 +13,6 @@ return new class extends Migration {
         Schema::create('glossary_page_views', function (Blueprint $table) {
             $table->id();
             $table->string('user_agent', 255);
-            $table->string('ip_address', 45);
             $table->string('browser', 45);
             $table->string('title')->nullable();
 
