707ff6f70faf853110b3f313995bf23359486222
Azizullah Saeidi <azizullahsaeidi@gmail.com>
2024-08-20 16:05:46 +0430

    Change resource view to SitewidePageView

--- app/Http/Controllers/ImpactController.php
+++ app/Http/Controllers/ImpactController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App;
 use App\Models\Resource;
-use App\Models\ResourceView;
+use App\Models\SitewidePageView;
 use App\Models\TaxonomyTerm;
 use Illuminate\View\View;
 
@@ -12,11 +12,9 @@ class ImpactController extends Controller
 {
     public function index(): View
     {
-        //total resources in number for the dashboard
         $totalResources = Resource::count();
-        $myresourceView = new ResourceView;
         $totalSubjects = TaxonomyTerm::where('vid', 8)->where('language', App::getLocale())->count();
-        $monthlyViews = $myresourceView->where('created_at', '>', \Carbon\Carbon::now()->subDays(30))->whereNotNull('platform')->count();
+        $monthlyViews = SitewidePageView::where('created_at', '>', \Carbon\Carbon::now()->subDays(30))->where('is_bot', false)->count();
 
         return view('impact.impact_page', compact('totalResources', 'monthlyViews', 'totalSubjects'));
     }
