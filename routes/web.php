bf65f032cf4fc04603e2bfc1968979c94f8e2ab9
Azizullah Saeidi <azizullahsaeidi@gmail.com>
2024-09-12 16:00:35 +0430

    Update the routes

--- routes/web.php
+++ routes/web.php
@@ -198,10 +198,10 @@ Route::prefix(LaravelLocalization::setLocale())->middleware('localeSessionRedire
     });
 
     //Glossary
-    Route::prefix('glossary')->middleware('LibraryManager')->group(function(){
-        Route::controller(GlossaryController::class)->group(function(){
-            Route::get('', 'index');
-            Route::post('', 'index')->name('glossary');
+    Route::prefix('glossary')->controller(GlossaryController::class)->group(function(){
+        Route::get('', 'index');
+        Route::post('', 'index')->name('glossary');
+        Route::prefix('glossary')->middleware('LibraryManager')->group(function(){
             Route::get('create', 'create')->name('glossary_create');
             Route::post('store', 'store')->name('glossary_store');
             Route::post('update', 'update')->name('glossary_update');
@@ -209,7 +209,7 @@ Route::prefix(LaravelLocalization::setLocale())->middleware('localeSessionRedire
             Route::post('approve/{id}', 'approve')->name('glossary_approve');
         });
     });
-    
+
     //Impact Page
     Route::get('/impact', [ImpactController::class, 'index']);
 
