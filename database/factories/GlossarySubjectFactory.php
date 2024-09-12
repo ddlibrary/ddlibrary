8dbaeb671e94a991a6439054fd7bcaf52bc60408
Azizullah Saeidi <azizullahsaeidi@gmail.com>
2024-09-12 12:40:13 +0430

    Update GlossarySubjectFactory

--- database/factories/GlossarySubjectFactory.php
+++ database/factories/GlossarySubjectFactory.php
@@ -2,11 +2,21 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\GlossarySubject;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\GlossarySubject>
+ */
 class GlossarySubjectFactory extends Factory
 {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = GlossarySubject::class;
+
     /**
      * Define the model's default state.
      *
@@ -15,7 +25,15 @@ class GlossarySubjectFactory extends Factory
     public function definition(): array
     {
         return [
-
+            'en' => $this->faker->name,
+            'fa' => $this->faker->name,
+            'ps' => $this->faker->name,
+            'pa' => $this->faker->name,
+            'mj' => $this->faker->name,
+            'no' => $this->faker->name,
+            'sh' => $this->faker->name,
+            'sw' => $this->faker->name,
+            'uz' => $this->faker->name,
         ];
     }
 }
