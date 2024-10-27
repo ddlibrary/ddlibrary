764337b64449788e0e23bcc4bf8896de40720beb
Azizullah Saeidi <azizullahsaeidi@gmail.com>
2024-10-27 20:35:15 +0430

    Add it_stores_resource_image

--- tests/Feature/Resource/UploadImageResourceStepOneTest.php
+++ tests/Feature/Resource/UploadImageResourceStepOneTest.php
@@ -2,12 +2,45 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 class UploadImageResourceStepOneTest extends TestCase
 {
     use RefreshDatabase;
 
-    
+    /** @test */
+    public function it_stores_resource_image()
+    {
+        Storage::fake('s3');
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $file = UploadedFile::fake()->image('image.jpg', 600, 600);
+
+        $response = $this->post('upload-image', $this->data(['image' => $file]));
+
+        $response->assertJson(['success' => true]);
+        $response->assertJsonStructure(['imageUuid', 'imageUrl', 'imageName', 'message']);
+
+        // Assert that the file was stored on S3
+        $fileName = auth()->user()->id . '_' . time() . '.jpg';
+        Storage::disk('s3')->assertExists('resources/' . $fileName);
+    }
+
+    protected function data($merge = [])
+    {
+        $file = UploadedFile::fake()->image('image.jpg', 200, 200);
+        return array_merge(
+            [
+                'license' => 'This is Title',
+                'image' => $file,
+                'image_name' => 'This is image name',
+            ],
+            $merge,
+        );
+    }
 }
