311598f2ba2da1787c2ffc956200de228abc7269
Azizullah Saeidi <azizullahsaeidi@gmail.com>
2024-10-27 18:30:38 +0430

    Create create_resource_files_table migration

---
+++ database/migrations/2024_10_14_094521_create_resource_files_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('resource_files', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->index();
+            $table->uuid('uuid')->unique();
+            $table->string('license')->nullable();
+            $table->string('path', 500);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('resource_files');
+    }
+};
