3623cd968bb25a6a4fca202b7237ca8c1e6c9309
Azizullah Saeidi <azizullahsaeidi@gmail.com>
2024-10-28 09:57:02 +0430

    Add new relationship

--- app/Models/ResourceFile.php
+++ app/Models/ResourceFile.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Str;
 
 class ResourceFile extends Model
@@ -19,5 +20,10 @@ class ResourceFile extends Model
         static::creating(function ($model) {
             $model->uuid = Str::uuid();
         });
+    }
+
+    public function resources(): HasMany
+    {
+        return $this->hasMany(Resource::class);
     }
 }
