9a057a82cb1a43833418c961416940c7fb327c75
Azizullah Saeidi <azizullahsaeidi@gmail.com>
2025-01-29 19:08:42 +0430

    Update impact data

--- tests/Feature/Http/Controllers/ImpactControllerTest.php
+++ tests/Feature/Http/Controllers/ImpactControllerTest.php
@@ -9,27 +9,36 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 /**
  * @see \App\Http\Controllers\ImpactController
  */
-        class ImpactControllerTest extends TestCase
-        {
-            use RefreshDatabase;
+class ImpactControllerTest extends TestCase
+{
+    use RefreshDatabase;
 
-            /**
-             * @test
-             */
-            public function index_returns_an_ok_response(): void
-            {
-                $this->refreshApplicationWithLocale('en');
-        
-                $response = $this->get(url("en/impact"));
-                
-                $response->assertOk();
-                $response->assertViewIs('impact.impact_page');
-                $response->assertViewHas('totalResources');
-                $response->assertViewHas('monthlyViews');
-                $response->assertViewHas('totalSubjects');
+    /**
+     * @test
+     */
+    public function index_returns_an_ok_response(): void
+    {
+        $this->refreshApplicationWithLocale('en');
 
-                $this->assertNotNull($response->viewData('totalResources'));
-                $this->assertNotNull($response->viewData('totalSubjects'));
-            }
+        $response = $this->get(url('en/impact'));
 
-        }
+        $response->assertOk();
+        $response->assertViewIs('impact.impact_page');
+        $response->assertViewHas('totalResources');
+        $response->assertViewHas('monthlyViews');
+        $response->assertViewHas('totalSubjects');
+
+        $this->assertNotNull($response->viewData('totalResources'));
+        $this->assertNotNull($response->viewData('totalSubjects'));
+
+        $admin = User::factory()->create();
+        $admin->roles()->attach(5);
+
+        $responseUpdate = $this->actingAs($admin)->get('en/impact?update=true');
+        $responseUpdate->assertOk();
+        $responseUpdate->assertViewIs('impact.impact_page');
+        $responseUpdate->assertViewHas('totalResources');
+        $responseUpdate->assertViewHas('monthlyViews');
+        $responseUpdate->assertViewHas('totalSubjects');
+    }
+}
