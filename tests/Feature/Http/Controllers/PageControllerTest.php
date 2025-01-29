7bf326072a55044ea13bc58d7ec7e2f07ee5f68d
Azizullah Saeidi <azizullahsaeidi@gmail.com>
2025-01-29 23:23:19 +0430

    body field is required

--- tests/Feature/Http/Controllers/PageControllerTest.php
+++ tests/Feature/Http/Controllers/PageControllerTest.php
@@ -261,6 +261,18 @@ class PageControllerTest extends TestCase
         $response->assertSessionHasErrors(['summary' => 'The summary field is required.']);
     }
 
+    /** @test */
+    public function body_field_is_required()
+    {
+        $this->refreshApplicationWithLocale('en');
+        $admin = User::factory()->create();
+        $admin->roles()->attach(5);
+
+        $response = $this->actingAs($admin)->post(route('add_page'), $this->data(['body' => '']));
+
+        $response->assertSessionHasErrors(['body' => 'The body field is required.']);
+    }
+
     protected function data($merge = [])
     {
         return array_merge(
