38589e843e5150d1d9d780c216a7306721729310
Azizullah Saeidi <azizullahsaeidi@gmail.com>
2025-01-29 23:25:47 +0430

    published field must be an integer on update

--- tests/Feature/Http/Controllers/PageControllerTest.php
+++ tests/Feature/Http/Controllers/PageControllerTest.php
@@ -348,6 +348,28 @@ class PageControllerTest extends TestCase
         $response->assertSessionHasErrors(['body' => 'The body field is required.']);
     }
 
+
+    /** @test */
+    public function update_published_field_must_be_an_integer()
+    {
+        $this->refreshApplicationWithLocale('en');
+
+        $admin = User::factory()->create();
+        $admin->roles()->attach(5);
+        $this->actingAs($admin);
+        $page = Page::factory()->create();
+
+        $response = $this->post(
+            route('update_page', ['pageId' => $page->id]),
+            $this->data([
+                'published' => 'text',
+                'summary' => 'Update summary.',
+            ]),
+        );
+
+        $response->assertSessionHasErrors(['published' => 'The published field must be an integer.']);
+    }
+
     protected function data($merge = [])
     {
         return array_merge(
