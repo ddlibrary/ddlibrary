70a45249c5383efd05f2387581cae0840b04f16e
Azizullah Saeidi <azizullahsaeidi@gmail.com>
2025-01-29 20:34:16 +0430

    weight field is required

--- tests/Feature/Http/Controllers/MenuControllerTest.php
+++ tests/Feature/Http/Controllers/MenuControllerTest.php
@@ -179,6 +179,18 @@ class MenuControllerTest extends TestCase
     }
 
     /** @test */
+    public function weight_field_is_required()
+    {
+        $this->refreshApplicationWithLocale('en');
+        $admin = User::factory()->create();
+        $admin->roles()->attach(5);
+
+        $response = $this->actingAs($admin)->post(route('store_menu'), $this->data(['weight' => '']));
+
+        $response->assertStatus(400);
+    }
+
+    /** @test */
     public function language_field_is_required()
     {
         $this->refreshApplicationWithLocale('en');
