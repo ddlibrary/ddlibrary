1dd970e66f4c92ceabc3d9446434a7984c1f5b92
Azizullah Saeidi <azizullahsaeidi@gmail.com>
2025-04-11 09:54:54 +0430

    Add linkedResource() on ResourceTranslationLink model

--- app/Models/ResourceTranslationLink.php
+++ app/Models/ResourceTranslationLink.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ResourceTranslationLink extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    public function linkedResource(): BelongsTo {
+        return $this->belongsTo(Resource::class, 'link_resource_id');
+    }
 }
