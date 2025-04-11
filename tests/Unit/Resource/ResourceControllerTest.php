13029ea054c44aa01a871e59abbaf4b153019c3e
Azizullah Saeidi <azizullahsaeidi@gmail.com>
2025-04-11 12:26:46 +0430

    Add it_does_not_add_a_resource_translation_link_if_already_exists() test

--- tests/Unit/Resource/ResourceControllerTest.php
+++ tests/Unit/Resource/ResourceControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\Resource;
 
 use App\Models\Resource;
+use App\Models\ResourceTranslationLink;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Session;
 use Tests\TestCase;
@@ -40,9 +41,43 @@ class ResourceControllerTest extends TestCase
             'resource_id' => $resource1->id,
             'link_resource_id' => $link->id,
         ]);
-        $this->assertEquals('Resource successfully added!', Session::get('alert.message'));
+        $this->assertEquals('Resource successfully linked!', Session::get('alert.message'));
         $this->assertEquals('success', Session::get('alert.level'));
     }
 
+    /** @test */
+    public function it_does_not_add_a_resource_translation_link_if_already_exists()
+    {
+        $this->refreshApplicationWithLocale('en');
+        $admin = User::factory()->create();
+        $admin->roles()->attach(5);
+        $resource1 = Resource::create([
+            'title' => "Resource 1",
+            'abstract' => "abstract content",
+            'language' => 'en',
+            'status' => 1,
+        ]);
+        $link = Resource::create([
+            'title' => "Resource 2",
+            'abstract' => "abstract content",
+            'language' => 'en',
+            'status' => 1,
+        ]);
+
+        ResourceTranslationLink::insert([
+            'resource_id' => $resource1->id,
+            'link_resource_id' => $link->id,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        $$response = $this->actingAs($admin)->post(route('updatetid', $resource1->id), [
+            'link_resource_id' => $link->id,
+        ]);
+
+        $this->assertDatabaseCount('resource_translation_links', 1);
+        $this->assertEquals('This resource already linked!', Session::get('alert.message'));
+        $this->assertEquals('danger', Session::get('alert.level'));
+    }
     
 }
