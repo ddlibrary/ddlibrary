ee638a4679d2da12cd62453d8ccf15da9ff2259d
Azizullah Saeidi <azizullahsaeidi@gmail.com>
2025-07-15 14:01:59 +0430

    Add delete icon on input form

--- resources/views/resources/resources_modify_step2.blade.php
+++ resources/views/resources/resources_modify_step2.blade.php
@@ -6,45 +6,61 @@
     <div class="container mt-3">
         <h3>@lang('Create or edit a resource - step 2 of 3')</h3>
         <hr>
-        <form method="POST" action="@if($edit){{ route('edit2', $resource['id']) }}@else{{ route('step2') }}@endif" enctype="multipart/form-data">
+        <form method="POST"
+            action="@if ($edit) {{ route('edit2', $resource['id']) }}@else{{ route('step2') }} @endif"
+            enctype="multipart/form-data">
             @csrf
-            <div class="form-group">
-                <label for="attachments">
-                    @lang('Attachments')
-                </label>
-                <input class="form-control-file{{ $errors->has('attachments') ? ' is-invalid' : '' }}col-md-6"
-                       id="attachments"
-                       name="attachments[]"
-                       type="file"
-                >
-                <button type='button' class="add_more btn btn-link">@lang('Add more files')</button>
-                @if(isset($resource['attc']) and $edit)
-                    @foreach($resource['attc'] as $item)
-                        <br><a href="{{ asset('/storage/attachments/'.$item['file_name']) }}">{{ $item['file_name'] }}</a>
-                    @endforeach
-                @endif
+            <div class=" col-6">
+                <div class="form-group">
+                    <label for="attachments">
+                        @lang('Attachments')
+                    </label>
+                    <div class="d-flex gap-3 attachment-1">
+                        <div class="flex-grow-1 align-items-center">
+                            <input
+                                class="form-control form-control-file {{ $errors->has('attachments') ? ' is-invalid' : '' }}"
+                                id="attachments" name="attachments[]" type="file">
+                        </div>
+                        <div class="align-self-center">
+                            <span class="fa fa-trash text-danger" onclick="removeAttachment('attachment-1')"></span>
+                        </div>
+                    </div>
+                    <button type='button' class="add_more btn btn-link">@lang('Add more files')</button>
+                    @if (isset($resource['attc']) and $edit)
+                        @foreach ($resource['attc'] as $item)
+                            <br><a
+                                href="{{ asset('/storage/attachments/' . $item['file_name']) }}">{{ $item['file_name'] }}</a>
+                        @endforeach
+                    @endif
 
-                @if ($errors->has('attachments'))
-                    <span class="invalid-feedback">
-                    <strong>{{ $errors->first('attachments') }}</strong>
-                </span><br>
-                @endif
+                    @if ($errors->has('attachments'))
+                        <span class="invalid-feedback">
+                            <strong>{{ $errors->first('attachments') }}</strong>
+                        </span><br>
+                    @endif
+
+                </div>
             </div>
             <div class="form-item col-6">
                 <label for="subject_areas">
                     <strong>@lang('Subject Areas') {{ en('Subject Areas') }}</strong>
                 </label>
-                <select class="form-select{{ $errors->has('subject_areas') ? ' is-invalid' : '' }}" id="subject_areas" name="subject_areas[]" size="10" required multiple>
-                    @foreach ($subjects AS $item)
-                        @if($item->parent == 0)
+                <select class="form-select{{ $errors->has('subject_areas') ? ' is-invalid' : '' }}" id="subject_areas"
+                    name="subject_areas[]" size="10" required multiple>
+                    @foreach ($subjects as $item)
+                        @if ($item->parent == 0)
                             <optgroup label="{{ $item->name }}">
-                                <option value="{{ $item->id }}" {{ $resourceSubjectAreas != null ? (in_array($item->id, $resourceSubjectAreas) ? 'selected' : '') : '' }}>{{ $item->name }}</option>
-                                    <?php if (isset($subjects) && isset($item)) {
-                                        $parentItems = $subjects->where('parent', $item->id);
-                                    }
-                                    ?>
-                                @foreach($parentItems as $pitem)
-                                    <option value="{{ $pitem->id }}" {{ $resourceSubjectAreas != null ? (in_array($pitem->id, $resourceSubjectAreas) ? 'selected' : '') : '' }}>{{ $pitem->name . termEn($pitem->id) }}</option>
+                                <option value="{{ $item->id }}"
+                                    {{ $resourceSubjectAreas != null ? (in_array($item->id, $resourceSubjectAreas) ? 'selected' : '') : '' }}>
+                                    {{ $item->name }}</option>
+                                <?php if (isset($subjects) && isset($item)) {
+                                    $parentItems = $subjects->where('parent', $item->id);
+                                }
+                                ?>
+                                @foreach ($parentItems as $pitem)
+                                    <option value="{{ $pitem->id }}"
+                                        {{ $resourceSubjectAreas != null ? (in_array($pitem->id, $resourceSubjectAreas) ? 'selected' : '') : '' }}>
+                                        {{ $pitem->name . termEn($pitem->id) }}</option>
                                 @endforeach
                             </optgroup>
                         @endif
@@ -53,54 +69,62 @@
 
                 @if ($errors->has('subject_areas'))
                     <span class="invalid-feedback">
-                    <strong>{{ $errors->first('subject_areas') }}</strong>
-                </span><br>
+                        <strong>{{ $errors->first('subject_areas') }}</strong>
+                    </span><br>
                 @endif
             </div>
             <div class="form-item col-6">
                 <label for="keywords">
                     <strong>@lang('Keywords') {{ en('Keywords') }}</strong>
                 </label>
-                <input class="form-control{{ $errors->has('keywords') ? ' is-invalid' : '' }}" id="keywords" name="keywords" type="text" value="{{ isset($resource['keywords'])?$resource['keywords']:"" }}" onkeydown="javascript:bringMeAttr('keywords','{{ URL::to('resources/attributes/keywords') }}')">
+                <input class="form-control{{ $errors->has('keywords') ? ' is-invalid' : '' }}" id="keywords"
+                    name="keywords" type="text" value="{{ isset($resource['keywords']) ? $resource['keywords'] : '' }}"
+                    onkeydown="javascript:bringMeAttr('keywords','{{ URL::to('resources/attributes/keywords') }}')">
                 <small id="authorOptional" class="form-text text-muted">
                     @lang('Optional')
                 </small>
                 @if ($errors->has('keywords'))
                     <span class="invalid-feedback">
-                    <strong>{{ $errors->first('keywords') }}</strong>
-                </span><br>
+                        <strong>{{ $errors->first('keywords') }}</strong>
+                    </span><br>
                 @endif
             </div>
             <div class="form-item col-6">
                 <label for="learning_resources_types">
                     <strong>@lang('Learning Resources Types') {{ en('Learning Resources Types') }}</strong>
                 </label>
-                <select class="form-control{{ $errors->has('learning_resources_types') ? ' is-invalid' : '' }}" id="learning_resources_types" name="learning_resources_types[]" size="10" required  multiple>
-                    @foreach ($learningResourceTypes AS $item)
-                        <option value="{{ $item->id }}" {{ $resourceLearningResourceTypes != null ? (in_array($item->id, $resourceLearningResourceTypes) ? 'selected' : '') : '' }}>{{ $item->name . termEn($item->id) }}</option>
+                <select class="form-control{{ $errors->has('learning_resources_types') ? ' is-invalid' : '' }}"
+                    id="learning_resources_types" name="learning_resources_types[]" size="10" required multiple>
+                    @foreach ($learningResourceTypes as $item)
+                        <option value="{{ $item->id }}"
+                            {{ $resourceLearningResourceTypes != null ? (in_array($item->id, $resourceLearningResourceTypes) ? 'selected' : '') : '' }}>
+                            {{ $item->name . termEn($item->id) }}</option>
                     @endforeach
                 </select>
 
                 @if ($errors->has('learning_resources_types'))
                     <span class="invalid-feedback">
-                    <strong>{{ $errors->first('learning_resources_types') }}</strong>
-                </span><br>
+                        <strong>{{ $errors->first('learning_resources_types') }}</strong>
+                    </span><br>
                 @endif
             </div>
             <div class="form-item col-6">
                 <label for="educational_use">
                     <strong>@lang('Educational Use') {{ en('Educational Use') }}</strong>
                 </label>
-                <select class="form-control{{ $errors->has('educational_use') ? ' is-invalid' : '' }}" id="educational_use" name="educational_use[]" size="5" required  multiple>
-                    @foreach ($educationalUse AS $item)
-                        <option value="{{ $item->id }}" {{ $editEducationalUse != null ? (in_array($item->id, $editEducationalUse) ? 'selected' : '') : '' }}>{{ $item->name . termEn($item->id) }}</option>
+                <select class="form-control{{ $errors->has('educational_use') ? ' is-invalid' : '' }}" id="educational_use"
+                    name="educational_use[]" size="5" required multiple>
+                    @foreach ($educationalUse as $item)
+                        <option value="{{ $item->id }}"
+                            {{ $editEducationalUse != null ? (in_array($item->id, $editEducationalUse) ? 'selected' : '') : '' }}>
+                            {{ $item->name . termEn($item->id) }}</option>
                     @endforeach
                 </select>
 
                 @if ($errors->has('educational_use'))
                     <span class="invalid-feedback">
-                    <strong>{{ $errors->first('educational_use') }}</strong>
-                </span><br>
+                        <strong>{{ $errors->first('educational_use') }}</strong>
+                    </span><br>
                 @endif
             </div>
 
@@ -110,26 +134,37 @@
                 </label>
                 <ul style="list-style-type: none;">
                     <?php
-                    if(!isset($resource['level'])){
+                    if (!isset($resource['level'])) {
                         $resource['level'] = [];
                     }
                     ?>
-                    @foreach($levels AS $level)
-                        @if($level->parent == 0)
-                            <li><input type="checkbox" name="level[]" {{ $resourceLevels != null ? (in_array($level->id, $resourceLevels ) ? "checked" : "") : ""}} value="{{ $level->id }}" onchange="fnTest(this,'subLevel{{$level->id}}');"> {{ $level->name . termEn($level->id) }}
-                                    <?php if (isset($levels) && isset($level)) {
+                    @foreach ($levels as $level)
+                        @if ($level->parent == 0)
+                            <li><input type="checkbox" name="level[]"
+                                    {{ $resourceLevels != null ? (in_array($level->id, $resourceLevels) ? 'checked' : '') : '' }}
+                                    value="{{ $level->id }}" onchange="fnTest(this,'subLevel{{ $level->id }}');">
+                                {{ $level->name . termEn($level->id) }}
+                                <?php if (isset($levels) && isset($level)) {
                                     $levelParent = $levels->where('parent', $level->id);
-                                }?>
-                                @if($levelParent)
-                                    <ul id="subLevel{{$level->id}}" class="subItem" style="list-style-type: none;">
-                                        @foreach($levelParent as $item)
-                                            <li><input type="checkbox" name="level[]" onchange="fnTest(this,'subLevel{{$item->id}}');" {{  $resourceLevels != null ? (in_array($item->id, $resourceLevels) ? "checked" : "") : ""}} class="js-child" value="{{ $item->id }}"> {{ $item->name  . termEn($item->id)}}
+                                } ?>
+                                @if ($levelParent)
+                                    <ul id="subLevel{{ $level->id }}" class="subItem" style="list-style-type: none;">
+                                        @foreach ($levelParent as $item)
+                                            <li><input type="checkbox" name="level[]"
+                                                    onchange="fnTest(this,'subLevel{{ $item->id }}');"
+                                                    {{ $resourceLevels != null ? (in_array($item->id, $resourceLevels) ? 'checked' : '') : '' }}
+                                                    class="js-child" value="{{ $item->id }}">
+                                                {{ $item->name . termEn($item->id) }}
 
-                                                    <?php $levelItemParent = $levels->where('parent', $item->id);?>
-                                                @if($levelItemParent)
-                                                    <ul id="subLevel{{$item->id}}" class="subItem" style="list-style-type: none;">
-                                                        @foreach($levelItemParent as $itemLevel)
-                                                            <li><input type="checkbox" name="level[]" {{  $resourceLevels != null ? (in_array($itemLevel->id, $resourceLevels) ? "checked" : "") : ""}} class="js-child" value="{{ $itemLevel->id }}"> {{ $itemLevel->name  . termEn($itemLevel->id)}}</li>
+                                                <?php $levelItemParent = $levels->where('parent', $item->id); ?>
+                                                @if ($levelItemParent)
+                                                    <ul id="subLevel{{ $item->id }}" class="subItem"
+                                                        style="list-style-type: none;">
+                                                        @foreach ($levelItemParent as $itemLevel)
+                                                            <li><input type="checkbox" name="level[]"
+                                                                    {{ $resourceLevels != null ? (in_array($itemLevel->id, $resourceLevels) ? 'checked' : '') : '' }}
+                                                                    class="js-child" value="{{ $itemLevel->id }}">
+                                                                {{ $itemLevel->name . termEn($itemLevel->id) }}</li>
                                                         @endforeach
                                                     </ul>
                                                 @endif
@@ -144,9 +179,20 @@
             </div>
             @include('layouts.messages')
             <div class="d-grid gap-2 d-md-block">
-                <input class="btn btn-primary btn-md" type="button" value="@lang('Previous') {{ en('Previous') }}" onclick="location.href='@if($edit){{ route('edit1', $resource['id']) }}@else{{ route('step1') }}@endif'">
+                <input class="btn btn-primary btn-md" type="button" value="@lang('Previous') {{ en('Previous') }}"
+                    onclick="location.href='@if ($edit) {{ route('edit1', $resource['id']) }}@else{{ route('step1') }} @endif'">
                 <input class="btn btn-primary btn-md" type="submit" value="@lang('Next') {{ en('Next') }}">
             </div>
         </form>
     </div>
 @endsection
+@push('scripts')
+    
+    <script>
+        function removeAttachment(attachment){
+            if(confirm("Are you sure to remove this file?")){
+                $(`.${attachment}`).remove()
+            }
+        }
+    </script>
+@endpush
