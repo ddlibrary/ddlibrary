136e3420b742c2dcfdcce9e68b543a91641dbc34
Azizullah <azizullahsaeidi@gmail.com>
2025-08-21 23:04:08 +0430

    Refactor file-list partial view

--- resources/views/resources/partial/file-list.blade.php
+++ resources/views/resources/partial/file-list.blade.php
@@ -1,23 +1,26 @@
 @if (count($files))
-    <div class="image-box">
+    <div class="row g-2">
         @foreach ($files as $file)
-            <div class="image-item" style="position:relative;" data-uuid="{{ $file->uuid }}"
-                data-url="{{ $file->path }}">
-                <div class="image-container responsive-square">
-                    <img src="{{ $file->thumbnail_path }}" alt="{{ $file->name }}" class="resource-cover">
+            <div class="col-6 col-sm-4 col-md-3" data-uuid="{{ $file->uuid }}"
+                onclick="selectImage('{{ $file->uuid }}', '{{ $file->path }}')">
+                <div
+                    class="position-relative border border-secondary rounded overflow-hidden w-100  h-100 image-{{ $file->uuid }}">
+                    <div class="ratio ratio-1x1">
+                        <img src="{{ $file->thumbnail_path }}" alt="{{ $file->name }}" class="img-fluid">
+                    </div>
+                    <p class="text-center mb-0">{{ $file->name }}</p>
                 </div>
-                <p class="image-name">{{ $file->name }}</p>
             </div>
         @endforeach
     </div>
 @else
-    <div style="border:1px solid lightgray; border-radius:5px; padding: 20px;">
-        <h2 class="text-center text-red">@lang('Resource not found')</h2>
+    <div class="border border-secondary rounded p-3 w-100">
+        <h2 class="text-center text-danger">@lang('Resource not found')</h2>
     </div>
 @endif
 
 @if ($files->hasPages())
-    <div class="pagination-container">
+    <div class="pagination-container mt-3">
         {{ $files->links() }}
     </div>
 @endif
