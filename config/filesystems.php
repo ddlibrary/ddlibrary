81a2b6c1a856fbe51cc440d3e793a423197f2474
Azizullah <azizullahsaeidi@gmail.com>
2025-09-18 12:32:45 +0430

    Reverted the changes

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,8 +38,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => storage_path('app/public/files'),
+            'url' => env('APP_URL').'/storage/files',
             'visibility' => 'public',
             'throw' => false,
         ],
