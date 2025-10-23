9be73cb665cfedcdbdcd94a1ebc741d82fb35c9e
Azizullah <azizullahsaeidi@gmail.com>
2025-10-23 18:26:42 +0430

    display resource_files.path instead of resources.image

--- resources/views/resources/resources_view.blade.php
+++ resources/views/resources/resources_view.blade.php
@@ -7,7 +7,7 @@
 @endsection
 @section('page_image')
 
-        {{  $resource->image ?  $resource->image  : getImagefromResource($resource->abstract, '282x254') }}
+        {{  $resource->resourceFile ?  $resource->resourceFile->path  : getImagefromResource($resource->abstract, '282x254') }}
 @endsection
 <style>
     .epub-container {
@@ -320,7 +320,7 @@
             <div class="col-md-3">
                 <div class="row">
                     <div class="p-3">
-                        <img class="resource-view-img" src="{{ getImagefromResource($resource->abstract, '282x254') }}"
+                        <img class="resource-view-img" src=" {{ $resource->resourceFile ?  $resource->resourceFile->path  : getImagefromResource($resource->abstract, '282x254') }}"
                             alt="Resource Main Image">
                     </div>
                 </div>
