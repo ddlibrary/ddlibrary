0892be3be6dcc335bcd47a880b61a9b5e92789ae
Azizullah <azizullahsaeidi@gmail.com>
2025-11-13 14:29:45 +0430

    Complete PrivacyPolicyControllerTest

---
+++ tests/Feature/Http/Controllers/PrivacyPolicyControllerTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class PrivacyPolicyControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @test
+     */
+    public function privacy_policy_returns_english_view(): void
+    {
+        $this->refreshApplicationWithLocale('en');
+
+        $response = $this->get('en/privacy-policy');
+
+        $response->assertOk();
+        $response->assertViewIs('privacy-policy.english');
+    }
+
+    /**
+     * @test
+     */
+    public function privacy_policy_returns_pashto_view(): void
+    {
+        $this->refreshApplicationWithLocale('ps');
+
+        $response = $this->get('ps/privacy-policy');
+
+        $response->assertOk();
+        $response->assertViewIs('privacy-policy.pashto');
+    }
+
+    /**
+     * @test
+     */
+    public function privacy_policy_defaults_to_farsi_view_for_other_locales(): void
+    {
+        $this->refreshApplicationWithLocale('fa');
+
+        $response = $this->get('fa/privacy-policy');
+
+        $response->assertOk();
+        $response->assertViewIs('privacy-policy.farsi');
+    }
+
+    /**
+     * @test    
+     */
+    public function privacy_policy_returns_farsi_with_other_locales_view(): void
+    {
+        $this->refreshApplicationWithLocale('uz');
+
+        $response = $this->get('uz/privacy-policy');
+
+        $response->assertOk();
+        $response->assertViewIs('privacy-policy.farsi');
+    }
+
+    /**
+     * @test
+     */
+    public function mobile_privacy_policy_returns_expected_view(): void
+    {
+        $this->refreshApplicationWithLocale('en');
+
+        $response = $this->get('en/mobile-privacy-policy');
+
+        $response->assertOk();
+        $response->assertViewIs('privacy-policy.mobile');
+    }
+}
+
