85efc03f4144a50d9915d850b08ce0a93e3de578
Azizullah <azizullahsaeidi@gmail.com>
2025-12-02 22:12:45 +0430

    Update the webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,6 @@
 let mix = require('laravel-mix');
+const fs = require('fs');
+const path = require('path');
 
 /*
  |--------------------------------------------------------------------------
@@ -14,6 +16,21 @@ mix.sass('resources/assets/sass/app.scss', 'public/css/all.css')
     .js('resources/assets/js/ddl.js', 'public/js/all.js')
     .js('resources/assets/js/epub.js', 'public/js/epub.js')
     .js('resources/assets/js/resource.js', 'public/js/resource.js')
-    .copyDirectory('vendor/tinymce/tinymce', 'public/js/tinymce')
     .js('resources/assets/js/tinymce.js', 'public/js/tinymce.js')
-    ;
+    .copyDirectory('vendor/tinymce/tinymce', 'public/js/tinymce')
+    .then(() => {
+        // Clean up manifest: remove TinyMCE directory entries
+        const manifestPath = path.resolve(__dirname, 'public/mix-manifest.json');
+        if (fs.existsSync(manifestPath)) {
+            const manifest = JSON.parse(fs.readFileSync(manifestPath, 'utf8'));
+            const filteredManifest = {};
+
+            for (const [key, value] of Object.entries(manifest)) {
+                if (!key.startsWith('/js/tinymce/')) {
+                    filteredManifest[key] = value;
+                }
+            }
+
+            fs.writeFileSync(manifestPath, JSON.stringify(filteredManifest, null, 4));
+        }
+    });
