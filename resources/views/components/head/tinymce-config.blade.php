523e965e9fc72947d7755eaa45fba8d334b7fe93
Azizullah <azizullahsaeidi@gmail.com>
2025-12-16 12:46:40 +0430

    Remove jquery

--- resources/views/components/head/tinymce-config.blade.php
+++ resources/views/components/head/tinymce-config.blade.php
@@ -1,6 +1,3 @@
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"
-    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-
 <script src="{{ asset('js/tinymce/tinymce.min.js') }}" referrerpolicy="origin"></script>
 
 <script>
