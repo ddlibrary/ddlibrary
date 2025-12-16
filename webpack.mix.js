dd90d4e822786d8375c1052c0c11a1184260393b
Azizullah <azizullahsaeidi@gmail.com>
2025-12-16 16:21:02 +0430

    Remove then()

--- webpack.mix.js
+++ webpack.mix.js
@@ -17,20 +17,4 @@ mix.sass('resources/assets/sass/app.scss', 'public/css/all.css')
     .js('resources/assets/js/epub.js', 'public/js/epub.js')
     .js('resources/assets/js/resource.js', 'public/js/resource.js')
     .js('resources/assets/js/tinymce.js', 'public/js/tinymce.js')
-    .copyDirectory('vendor/tinymce/tinymce', 'public/js/tinymce')
-    .then(() => {
-        // Clean up manifest: remove TinyMCE directory entries
-        const manifestPath = path.resolve(__dirname, 'public/mix-manifest.json');
-        if (fs.existsSync(manifestPath)) {
-            const manifest = JSON.parse(fs.readFileSync(manifestPath, 'utf8'));
-            const filteredManifest = {};
-
-            for (const [key, value] of Object.entries(manifest)) {
-                if (!key.startsWith('/js/tinymce/')) {
-                    filteredManifest[key] = value;
-                }
-            }
-
-            fs.writeFileSync(manifestPath, JSON.stringify(filteredManifest, null, 4));
-        }
-    });
+    .copyDirectory('vendor/tinymce/tinymce', 'public/js/tinymce');
