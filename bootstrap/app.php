d8e7881d205c860f3bc6ea4e533b613a48385d5a
Shift <shift@laravelshift.com>
2026-01-07 06:34:22 +0000

    Re-register HTTP middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,25 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo(fn () => route('login'));
+        $middleware->redirectUsersTo('/home');
+
+        $middleware->validateCsrfTokens(except: [
+            '/laravel-filemanager/upload',
+        ]);
+
+        $middleware->throttleApi();
+
+        $middleware->alias([
+            'LibraryManager' => \App\Http\Middleware\LibraryManager::class,
+            'NormalUser' => \App\Http\Middleware\NormalUser::class,
+            'admin' => \App\Http\Middleware\RedirectIfAdmin::class,
+            'localeCookieRedirect' => \Mcamara\LaravelLocalization\Middleware\LocaleCookieRedirect::class,
+            'localeSessionRedirect' => \Mcamara\LaravelLocalization\Middleware\LocaleSessionRedirect::class,
+            'localeViewPath' => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationViewPath::class,
+            'localizationRedirect' => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRedirectFilter::class,
+            'localize' => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRoutes::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
