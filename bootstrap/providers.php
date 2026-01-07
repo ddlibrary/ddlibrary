57a5ffe0893d1fe81fcf1d28c49e2cedc2c1f0a4
Shift <shift@laravelshift.com>
2026-01-07 06:34:22 +0000

    Re-register service providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\ComposerServiceProvider::class,
+    App\Providers\RouteServiceProvider::class,
+    App\Providers\FacebookServiceProvider::class,
 ];
