26bed29447b2b9da23004179ee5e08ee18684e6f
Shift <shift@laravelshift.com>
2026-01-07 06:34:05 +0000

    Slim `lang` files

--- lang/en/passwords.php
+++ lang/en/passwords.php
@@ -4,7 +4,7 @@ return [
     'password' => 'Passwords must be at least six characters and match the confirmation.',
     'reset' => 'Your password has been reset!',
     'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
+
     'user' => 'if the user exists in our system, we will send an email',
-    'throttled' => 'Please wait before retrying.',
+
 ];
