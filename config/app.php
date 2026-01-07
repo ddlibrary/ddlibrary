4fed0552e881e8b23bc0475a431a152c751fec81
Shift <shift@laravelshift.com>
2026-01-07 06:34:22 +0000

    Re-register service providers

--- config/app.php
+++ config/app.php
@@ -11,24 +11,6 @@ return [
 
     'facebook_sso_enabled' => env('FACEBOOK_SSO_ENABLED', false),
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Package Service Providers...
-         */
-        'Barryvdh\TranslationManager\ManagerServiceProvider',
-        Yajra\Datatables\DatatablesServiceProvider::class,
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        App\Providers\BroadcastServiceProvider::class,
-        App\Providers\ComposerServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-        App\Providers\FacebookServiceProvider::class,
-    ])->toArray(),
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
