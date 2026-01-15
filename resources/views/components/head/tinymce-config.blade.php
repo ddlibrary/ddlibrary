3d516ce341c1b97599b33b1a30ac65816dc18bb7
Azizullah <azizullahsaeidi@gmail.com>
2026-01-15 23:01:22 +0430

    Ensure TinyMCE is fully loaded before initializing

--- resources/views/components/head/tinymce-config.blade.php
+++ resources/views/components/head/tinymce-config.blade.php
@@ -2,6 +2,16 @@
 
 <script>
     var filebrowserImageUploadUrl = "{{ route('upload.image.from.editor') }}";
+    
+    // Ensure TinyMCE is fully loaded before initializing
+    if (typeof tinymce === 'undefined') {
+        // Wait for TinyMCE to load
+        var checkTinyMCE = setInterval(function() {
+            if (typeof tinymce !== 'undefined' && typeof tinymce.init === 'function') {
+                clearInterval(checkTinyMCE);
+            }
+        }, 50);
+    }
 </script>
 
-<script src="{{ asset('js/tinymce.js') }}" referrerpolicy="origin"></script>
+<script src="{{ asset('js/tinymce.js') }}?v={{ filemtime(public_path('js/tinymce.js')) }}" referrerpolicy="origin"></script>
