6bc1a04544d332c443ac13f1119e5ca8416076fd
uyscuti-wiki <uyscuti.wiki@gmail.com>
2026-01-16 19:34:49 +0530

    fix grammar in reCAPTCHA error message

--- lang/en/validation.php
+++ lang/en/validation.php
@@ -3,12 +3,9 @@
 return [
 
     'custom' => [
-        'attribute-name' => [
-
-        ],
         'g-recaptcha-response' => [
             'required' => 'Please verify that you are not a robot.',
-            'captcha' => 'Captcha error! try again later or contact site admin.',
+            'captcha' => 'Captcha error! Try again later or contact site admin.',
         ],
     ],
 
