d238f40b21895fc8e934a7bacd18c9101d30c53f
Azizullah <azizullahsaeidi@gmail.com>
2026-01-22 20:05:58 +0430

    Add news-form id for news_edit form

--- resources/views/news/news_edit.blade.php
+++ resources/views/news/news_edit.blade.php
@@ -6,7 +6,7 @@
     </header>
     <div class="content-body">
         @include('layouts.messages')
-        <form method="POST" action="{{ route('update_news', $news->id) }}">
+        <form method="POST" action="{{ route('update_news', $news->id) }}" id="news-form">
         @csrf
         <div class="form-item">
             <label for="title"> 
