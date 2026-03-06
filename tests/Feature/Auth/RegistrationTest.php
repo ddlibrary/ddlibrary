07885ca6a7739cb57d0be9004a869fd72609a898
uyscuti-wiki <uyscuti.wiki@gmail.com>
2026-03-06 11:29:22 +0100

    test

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -65,6 +65,7 @@ class RegistrationTest extends TestCase
     /** @test */
     public function name_field_is_required(): void
     {
+        dump('Before before data(): ' . \App\Models\User::count());
         $this->refreshApplicationWithLocale('en');
         $response = $this->from('en/register')->post(
             'en/register',
@@ -75,6 +76,7 @@ class RegistrationTest extends TestCase
 
         $response->assertRedirect('en/register');
         $response->assertSessionHasErrors('first_name');
+        dd('After data(): ' . \App\Models\User::count(), \App\Models\User::first()->toArray());
         $this->assertNull(User::first());
     }
 
