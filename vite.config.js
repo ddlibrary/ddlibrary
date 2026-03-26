c84e9121fa7a1b3185c6eed1b3aec28bef4db4df
uyscuti-wiki <uyscuti.wiki@gmail.com>
2026-03-26 14:05:53 +0100

    Update Vite configuration: add server host, alias for FontAwesome, and switch JavaScript files to JSX

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,26 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import * as path from "node:path";
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+    },
     plugins: [
         laravel({
             input: [
                 'resources/assets/sass/app.scss',
-                'resources/assets/js/ddl.js',
-                'resources/assets/js/epub.js',
-                'resources/assets/js/resource.js',
+                'resources/assets/js/ddl.jsx',
+                'resources/assets/js/epub.jsx',
+                'resources/assets/js/resource.jsx',
                 'resources/assets/js/tinymce.js',
             ],
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '../webfonts': path.resolve(__dirname, 'node_modules/@fortawesome/fontawesome-free/webfonts'),
+        },
+    },
 });
